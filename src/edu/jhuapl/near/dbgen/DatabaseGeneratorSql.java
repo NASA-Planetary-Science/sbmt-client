36f35351206ce927d817087ac892e8dbdfb25842
Eli Kahn <eliezer.kahn@jhuapl.edu>
2010-04-07 20:23:06 +0000

    removed faulty import
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@27410 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- src/edu/jhuapl/near/dbgen/DatabaseGeneratorSql.java
+++ src/edu/jhuapl/near/dbgen/DatabaseGeneratorSql.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.*;
 
-import edu.jhuapl.near.database.*;
 import edu.jhuapl.near.model.*;
 import edu.jhuapl.near.util.*;
 
