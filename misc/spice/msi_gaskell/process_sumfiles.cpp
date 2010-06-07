f25d22dbdcde2a4e9ba7ec4e7c2373506085df43
Eli Kahn <eliezer.kahn@jhuapl.edu>
2010-06-07 16:45:01 +0000

    more work on new msi sumfile processing
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@28806 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/msi_gaskell/process_sumfiles.cpp
+++ misc/spice/msi_gaskell/process_sumfiles.cpp
@@ -1,3 +1,31 @@
-int main()
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+std::vector<std::string> loadSumFileList(std::string sumfilelist)
 {
+
+}
+
+
+void loadSumFile(std::string sumfile,
+		 double cx[3],
+		 double cy[3],
+		 double cz[3])
+{
+
+}
+
+
+int main(int argc, char** argv)
+{
+  std::string sumfilelist = argv[1];
+  std::vector<std::string> sumfiles = loadSumFileList(sumfilelist);
+  
+  for (unsigned int i=0; i<sumfiles.size(); ++i)
+    {
+      
+    }
 }
