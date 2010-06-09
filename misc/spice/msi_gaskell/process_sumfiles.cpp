ee3c4c211ac38374165ef96ba9ea2c21a06e7b25
Eli Kahn <eliezer.kahn@jhuapl.edu>
2010-06-09 22:02:20 +0000

    fixed some spacing issues
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@28887 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/msi_gaskell/process_sumfiles.cpp
+++ misc/spice/msi_gaskell/process_sumfiles.cpp
@@ -79,8 +79,8 @@ void loadSumFile(const std::string& sumfile,
         std::getline(fin, utc);
         trim(utc);
 	
-	// Replace spaces with dashes in the utc string
-	std::replace(utc.begin(), utc.end(), ' ', '-');
+        // Replace spaces with dashes in the utc string
+        std::replace(utc.begin(), utc.end(), ' ', '-');
 
         for (int i=0; i<7; ++i) fin >> dummy;
         for (int i=0; i<5; ++i) fin >> dummy;
@@ -102,34 +102,35 @@ void loadSumFile(const std::string& sumfile,
         fin >> dummy;
         for (int i=0; i<4; ++i) fin >> dummy;
 
-        std::cout << utc << std::endl;
         utc2et_c(utc.c_str(), &et);
+
         std::cout << et << std::endl;
+        double msi_to_eros[3][3];
+        msi_to_eros[0][0] = cx[0];
+        msi_to_eros[0][1] = cx[1];
+        msi_to_eros[0][2] = cx[2];
+        msi_to_eros[1][0] = cy[0];
+        msi_to_eros[1][1] = cy[1];
+        msi_to_eros[1][2] = cy[2];
+        msi_to_eros[2][0] = cz[0];
+        msi_to_eros[2][1] = cz[1];
+        msi_to_eros[2][2] = cz[2];
 
-	double msi_to_eros[3][3];
-	msi_to_eros[0][0] = cx[0];
-	msi_to_eros[0][1] = cx[1];
-	msi_to_eros[0][2] = cx[2];
-	msi_to_eros[1][0] = cy[0];
-	msi_to_eros[1][1] = cy[1];
-	msi_to_eros[1][2] = cy[2];
-	msi_to_eros[2][0] = cz[0];
-	msi_to_eros[2][1] = cz[1];
-	msi_to_eros[2][2] = cz[2];
+        invert_c(msi_to_eros, msi_to_eros);
+        
+        double sc_bus_prime_to_msi[3][3];
+        pxform_c("NEAR_SC_BUS_PRIME", "NEAR_MSI", et, sc_bus_prime_to_msi);
 
-	double sc_bus_prime_to_msi[3][3];
-	pxform_c("NEAR_SC_BUS_PRIME", "NEAR_MSI", et, sc_bus_prime_to_msi);
+        double eros_to_j2000[3][3];
+        pxform_c("IAU_EROS", "J2000", et, eros_to_j2000);
 
-	double eros_to_j2000[3][3];
-	pxform_c("IAU_EROS", "J2000", et, eros_to_j2000);
+        double tmp[3][3];
+        mxm_c(eros_to_j2000, msi_to_eros, tmp);
 
-	double tmp[3][3];
-	mxm_c(eros_to_j2000, msi_to_eros, tmp);
+        double tmp2[3][3];
+        mxm_c(tmp, sc_bus_prime_to_msi, tmp2);
 
-	double tmp2[3][3];
-	mxm_c(tmp, sc_bus_prime_to_msi, tmp2);
-
-	invert_c(tmp2, mat);
+        invert_c(tmp2, mat);
     }
     else
     {
@@ -145,7 +146,6 @@ void loadSpiceKernels(const std::vector<std::string>& kernelfiles)
 {
     for (unsigned int i=0; i<kernelfiles.size(); ++i)
     {
-        std::cout << kernelfiles[i] << std::endl;
         furnsh_c(kernelfiles[i].c_str());
     }
 }
@@ -170,7 +170,7 @@ void createMsopckSetupFile()
     fout << "CK_TYPE                = 3\n";
     fout << "INSTRUMENT_ID          = -93000\n";
     fout << "REFERENCE_FRAME_NAME   = 'J2000'\n";
-    fout << "ANGULAR_RATE_PRESENT   = 'NO'\n";
+    fout << "ANGULAR_RATE_PRESENT   = 'MAKE UP/NO AVERAGING'\n";
     fout << "INPUT_TIME_TYPE        = 'UTC'\n";
     fout << "INPUT_DATA_TYPE        = 'MATRICES'\n";
     fout << "PRODUCER_ID            = 'E. Kahn, JHUAPL'\n";
@@ -194,7 +194,7 @@ void createMsopckInputDataFile(const std::vector<TimeMatrix>& data)
     for (unsigned int i=0; i<data.size(); ++i)
     {
         const TimeMatrix& tm = data[i];
-        fout << std::scientific << tm.utc << " ";
+        fout << tm.utc << " ";
         fout << std::scientific << tm.mat[0][0] << " ";
         fout << std::scientific << tm.mat[0][1] << " ";
         fout << std::scientific << tm.mat[0][2] << " ";
