49c0bb36767f7396ff2edf1548e74285f9cf418c
Eli Kahn <eliezer.kahn@jhuapl.edu>
2010-06-11 14:00:13 +0000

    changed caching procedure so that file is only downloaded if time stamp is newer than existing file
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@28939 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- src/edu/jhuapl/near/util/FileCache.java
+++ src/edu/jhuapl/near/util/FileCache.java
@@ -2,6 +2,7 @@ package edu.jhuapl.near.util;
 
 import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.zip.GZIPInputStream;
 
 public class FileCache 
@@ -17,15 +18,33 @@ public class FileCache
 			unzippedPath = unzippedPath.substring(0, unzippedPath.length()-3);
 		
 		File file = new File(Configuration.getCacheDir() + File.separator + unzippedPath);
-		if (file.exists())
+		
+		// Open a connection the file on the server
+		try 
 		{
-			return file;
+			URL u = new URL(Configuration.getDataRootURL() + path);
+			URLConnection conn = u.openConnection();
+
+			long fileLastModified = file.lastModified();
+			long urlLastModified = conn.getLastModified();
+
+			if (file.exists() && fileLastModified >= urlLastModified)
+			{
+				return file;
+			}
+			else
+			{
+				file = addToCache(path, conn.getInputStream(), urlLastModified);
+				return file;
+			}
 		}
-		else
+		catch (IOException e) 
 		{
-			file = addToCache(path);
-			return file;
+			file = null;
+			e.printStackTrace();
 		}
+
+		return null;
 	}
 	
 	//static private void deleteOldCaches()
@@ -36,44 +55,35 @@ public class FileCache
 	/**
 	 * When adding to cache, gzipped files are always uncompressed and saved
 	 * without the ".gz" extension.
+	 * @throws IOException 
 	 */
-	static private File addToCache(String path)
+	static private File addToCache(String path, InputStream is, long urlLastModified) throws IOException
 	{
-		File file = null;
-		try 
+		if (path.toLowerCase().endsWith(".gz"))
+			is = new GZIPInputStream(is);
+
+		if (path.toLowerCase().endsWith(".gz"))
+			path = path.substring(0, path.length()-3);
+
+		File file = new File(Configuration.getCacheDir() + File.separator + path);
+
+		file.getParentFile().mkdirs();
+
+		FileOutputStream os = new FileOutputStream(file);
+
+		byte[] buff = new byte[2048];
+		int len;
+		while((len = is.read(buff)) > 0)
 		{
-			URL u = new URL(Configuration.getDataRootURL() + path);
-			
-			InputStream is = u.openStream();
-			
-			if (path.toLowerCase().endsWith(".gz"))
-				is = new GZIPInputStream(is);
-
-			if (path.toLowerCase().endsWith(".gz"))
-				path = path.substring(0, path.length()-3);
-			
-			file = new File(Configuration.getCacheDir() + File.separator + path);
-			
-			file.getParentFile().mkdirs();
-			
-			FileOutputStream os = new FileOutputStream(file);
-
-			byte[] buff = new byte[2048];
-			int len;
-			while((len = is.read(buff)) > 0)
-			{
-				os.write(buff, 0, len);
-			}
-
-			os.close();
-			is.close();
-		} 
-		catch (IOException e) 
-		{
-			file = null;
-			e.printStackTrace();
+			os.write(buff, 0, len);
 		}
-		
+
+		os.close();
+		is.close();
+
+		if (urlLastModified > 0)
+			file.setLastModified(urlLastModified);
+
 		return file;	
 	}
 }
