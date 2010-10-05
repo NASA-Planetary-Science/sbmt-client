56fc8fb464a458d030fd658c92baacf582e7337e
Eli Kahn <eliezer.kahn@jhuapl.edu>
2010-10-05 13:40:18 +0000

    began work on new PolyDataModel which contains a single vtkPolyData object
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@30883 ca6fedb5-3f05-0410-a19e-b2f146938ed9

---
+++ src/edu/jhuapl/near/model/PolyDataModel.java
@@ -0,0 +1,17 @@
+package edu.jhuapl.near.model;
+
+import java.util.ArrayList;
+
+import vtk.vtkProp;
+
+public class PolyDataModel extends Model
+{
+
+	@Override
+	public ArrayList<vtkProp> getProps()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
