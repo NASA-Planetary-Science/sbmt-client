9f9152285df2bc0764d4991a311e8ab7174708b1
Eli Kahn <eliezer.kahn@jhuapl.edu>
2010-11-02 21:28:09 +0000

    began modifying vesta model generator to work with new shape models provided by Olivier
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@31502 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- src/edu/jhuapl/near/dbgen/VestaModelGenerator.java
+++ src/edu/jhuapl/near/dbgen/VestaModelGenerator.java
@@ -1,28 +1,166 @@
 package edu.jhuapl.near.dbgen;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import vtk.vtkCellArray;
-import vtk.vtkCleanPolyData;
-import vtk.vtkIdList;
-import vtk.vtkPoints;
+//import vtk.vtkCellArray;
+//import vtk.vtkCleanPolyData;
+//import vtk.vtkIdList;
+//import vtk.vtkPoints;
 import vtk.vtkPolyData;
 import vtk.vtkPolyDataNormals;
+import vtk.vtkPolyDataReader;
 import vtk.vtkPolyDataWriter;
 
 import edu.jhuapl.near.model.Graticule;
 import edu.jhuapl.near.model.ModelFactory;
-import edu.jhuapl.near.util.LatLon;
-import edu.jhuapl.near.util.MathUtil;
+//import edu.jhuapl.near.util.LatLon;
+//import edu.jhuapl.near.util.MathUtil;
 import edu.jhuapl.near.util.NativeLibraryLoader;
 
 public class VestaModelGenerator {
 
 	/**
+	 * This program converts the Hubble Vesta shape model
+	 *
+	 * In addition this program generates normals and saves it to the vtk file.
+	 * In addition this program generates a coordinate grid and saves it to a
+	 * vtk file.
+	 * 
+	 * To run this program, 2 arguments are required:
+	 * - the folder containing the 4vesta.tab file
+	 * - the folder where you want to save the generated files
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args)
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
+        {
+        	public void run()
+        	{
+        		// do nothing
+        	}
+        });
+		
+		NativeLibraryLoader.loadVtkLibrariesLinuxNoX11();
+
+		String datadir = args[0];
+		String outputdir = args[1];
+
+		String[] pltfiles = {"SHAPE"};
+
+		try
+		{
+			for (int i=0; i<1; ++i)
+			{
+				String filename = datadir + "/" + pltfiles[i] + ".PLT";
+
+				InputStream fs = new FileInputStream(filename);
+				InputStreamReader isr = new InputStreamReader(fs);
+				BufferedReader in = new BufferedReader(isr);
+
+				String vtkfile = outputdir + "/" + pltfiles[i] + ".vtk";
+				FileWriter fstream = new FileWriter(vtkfile);
+				BufferedWriter out = new BufferedWriter(fstream);
+
+				// Read in the first line which list the number of points and plates
+				String val = in.readLine().trim();
+				int numPoints = Integer.parseInt(val);
+
+				out.write("# vtk DataFile Version 2.0\n");
+				out.write("VESTA\n");
+				out.write("ASCII\n");
+				out.write("DATASET POLYDATA\n");
+				out.write("POINTS " + numPoints + " float\n");
+
+				for (int j=0; j<numPoints; ++j)
+				{
+					String[] vals = in.readLine().trim().split("\\s+");
+					out.write(vals[1] + " " + vals[2] + " " + vals[3] + "\n");
+				}
+
+				val = in.readLine().trim();
+				int numPlates = Integer.parseInt(val);
+
+				out.write("POLYGONS " + numPlates + " " + (numPlates*4) + "\n");
+
+				// Note that the point indices are 1-based in the Gaskell model but vtk
+				// requires 0-based. Therefore subract 1 from each index.
+				for (int j=0; j<numPlates; ++j)
+				{
+					String[] vals = in.readLine().trim().split("\\s+");
+					int idx1 = Integer.parseInt(vals[1]) - 1;
+					int idx2 = Integer.parseInt(vals[2]) - 1;
+					int idx3 = Integer.parseInt(vals[3]) - 1;
+					out.write("3 " + idx1 + " " + idx2 + " " + idx3 + "\n");
+				}
+
+				out.close();
+				in.close();
+
+				// Now load the vtk file, generate normals, and save as binary
+				vtkPolyDataReader vestaReader = new vtkPolyDataReader();
+				vestaReader.SetFileName(vtkfile);
+				vestaReader.Update();
+
+				vtkPolyDataNormals normalsFilter = new vtkPolyDataNormals();
+				normalsFilter.SetInputConnection(vestaReader.GetOutputPort());
+				normalsFilter.SetComputeCellNormals(0);
+				normalsFilter.SetComputePointNormals(1);
+				normalsFilter.SplittingOff();
+				normalsFilter.Update();
+
+				vtkPolyDataWriter writer = new vtkPolyDataWriter();
+				writer.SetInputConnection(normalsFilter.GetOutputPort());
+				writer.SetFileName(vtkfile);
+				writer.SetFileTypeToBinary();
+				writer.Write();
+
+				// Generate the coordinate grid
+				String gridfile = outputdir + "/coordinate_grid_res" + i + ".vtk";
+
+				vtkPolyData vestaPolyData = new vtkPolyData();
+				vestaPolyData.DeepCopy(vestaReader.GetOutput());
+
+				Graticule grid = ModelFactory.createVestaGraticuleModel(null);
+				grid.generateGrid(vestaPolyData);
+
+				writer = new vtkPolyDataWriter();
+				writer.SetInput(grid.getGridAsPolyData());
+				writer.SetFileName(gridfile);
+				writer.SetFileTypeToBinary();
+				writer.Write();
+
+				if (i == 0)
+				{
+					String coloringfile = datadir + "/VESTA_Elevation.txt";
+					ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
+					coloringfile = datadir + "/VESTA_GravitationalAcceleration.txt";
+					ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
+					coloringfile = datadir + "/VESTA_GravitationalPotential.txt";
+					ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
+					coloringfile = datadir + "/VESTA_Slope.txt";
+					ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	/*
+	
 	 * This program converts the Hubble Vesta shape model that can be downloaded
 	 * from http://sbn.psi.edu/pds/asteroid/EAR_A_5_DDR_SHAPE_MODELS_V2_1.zip to
 	 * vtk format. This program can convert the file:
@@ -38,7 +176,7 @@ public class VestaModelGenerator {
 	 * - the folder where you want to save the generated files
 	 * 
 	 * @param args
-	 */
+
 	public static void main(String[] args)
 	{
 		javax.swing.SwingUtilities.invokeLater(new Runnable()
@@ -183,5 +321,5 @@ public class VestaModelGenerator {
 		}
 
 	}
-
+*/
 }
