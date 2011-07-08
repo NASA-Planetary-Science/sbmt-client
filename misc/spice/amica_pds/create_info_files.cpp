eaa8edcfca078b897ada1adb53af935d6baf4987
Eli Kahn <eliezer.kahn@jhuapl.edu>
2011-07-08 13:16:19 +0000

    updated program for generating info files for each amica fit file to be used for creating a pds database as was done for msi images
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@37719 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/amica_pds/create_info_files.cpp
+++ misc/spice/amica_pds/create_info_files.cpp
@@ -2,31 +2,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <stdlib.h>
+#include <cstdlib>
 extern "C"
 {
 #include "SpiceUsr.h"
 }
 
 using namespace std;
-
-
-// Remove initial and trailing whitespace from string. Modifies string in-place
-void trim(string& s)
-{
-    const size_t si = s.find_first_not_of(" \t");
-    if (si != string::npos)
-    {
-        const size_t ei = s.find_last_not_of(" \t");
-        const size_t l = (ei == string::npos ? ei : ei - si + 1);
-        s = s.substr(si, l);
-    }
-    else
-    {
-        s = "";
-    }
-}
 
 
 vector<string> loadFileList(const string& filelist)
@@ -51,80 +33,157 @@ vector<string> loadFileList(const string& filelist)
 }
 
 
-void loadSumFile(const string& sumfile,
-                 string& utc,
-                 double& et)
+void getEt(const string& fitfile,
+           string& utc,
+           double& et)
 {
-    ifstream fin(sumfile.c_str());
+    ifstream fin(fitfile.c_str());
 
     if (fin.is_open())
     {
-        string name;
-        
-        getline(fin, name);
-        trim(name);
-
-        getline(fin, utc);
-        trim(utc);
-	
-        // Replace the third space with a T
-        utc[11] = 'T';
-
-        // Replace remaining spaces with dashes
-        replace(utc.begin(), utc.end(), ' ', '-');
-
-        // Replace the month abbreviation with an integer. Only SEP,
-        // OCT, and NOV are needed.
-        string month = utc.substr(5, 3);
-        if (month == "SEP")
-            month = "09";
-        else if (month == "OCT")
-            month = "10";
-        else if (month == "NOV")
-            month = "11";
-        else
+        string str;
+        while(true)
         {
-            cerr << "Error: month " << month << " not recognized" << endl;
-            exit(1);
+            fin >> str;
+            
+            if (str == "UTC_0")
+            {
+                fin >> str; // the equals character
+                fin >> str; // the utc string
+                break;
+            }
         }
-        
-        utc = utc.substr(0, 5) + month + utc.substr(8);
+
+        utc = str.substr(1, 19);
 
         utc2et_c(utc.c_str(), &et);
     }
     else
     {
-        cerr << "Error: Unable to open file '" << sumfile << "'" << endl;
+        cerr << "Error: Unable to open file '" << fitfile << "'" << endl;
         exit(1);
     }
 
     fin.close();
 }
 
+
 /*
-   This function computes the position of the sun in the Eros body frame.
+  This function
    
-   Input:
-     et:         Ephemeris time
+  Input:
+  et:         Ephemeris time
    
-   Output:
-     sunpos:     The position of the sun in body cooridinates
+  Output:
+  scposb:
+  boredir:
+  updir:
+  frustum:
+   
+*/
+void getScOrientation(double et, double scposb[3], double boredir[3], double updir[3],
+                      double frustum[12])
+{
+    double lt;
+    double i2bmat[3][3];
+    double vpxi[3];
+    double ci[3];
+    double zo,yo;
+    const char* target = "HAYABUSA";
+    const char* ref = "IAU_ITOKAWA";
+    const char* abcorr = "NONE";
+    const char* obs = "ITOKAWA";
+    const char* frame = "HAYABUSA_AMICA";
+ 
+    spkpos_c(target, et, ref, abcorr, obs, scposb, &lt);
+    if (failed_c())
+        return;
+    
+    cout.precision(16);
+    cout << "Spacecraft Position: " << scposb[0] << " " << scposb[1] << " " << scposb[2] << endl;
+
+    pxform_c(frame, ref, et, i2bmat);
+    if (failed_c())
+        return;
+
+    zo=-0.049782949;
+    yo=-0.049782949;
+
+
+    /* First compute the direction of the center pixel */
+    vpxi[0] = 1.0;
+    vpxi[1] = 0.0;
+    vpxi[2] = 0.0;
+    vpack_c(vpxi[0], vpxi[1], vpxi[2], ci);
+    mxv_c(i2bmat, ci, boredir);
+
+    /* Then compute the up direction */
+    vpxi[0] = 0.0;
+    vpxi[1] = 0.0;
+    vpxi[2] = 1.0;
+    vpack_c(vpxi[0], vpxi[1], vpxi[2], ci);
+    mxv_c(i2bmat, ci, updir);
+
+    /* Now compute the frustum */
+    vpxi[0] = 1.0;
+    vpxi[1] = yo;
+    vpxi[2] = zo;
+    vpack_c(vpxi[0], vpxi[1], vpxi[2], ci);
+    mxv_c(i2bmat, ci, &frustum[0]);
+
+    vpxi[0] = 1.0;
+    vpxi[1] = yo;
+    vpxi[2] = -zo;
+    vpack_c(vpxi[0], vpxi[1], vpxi[2], ci);
+    mxv_c(i2bmat, ci, &frustum[3]);
+
+    vpxi[0] = 1.0;
+    vpxi[1] = -yo;
+    vpxi[2] = zo;
+    vpack_c(vpxi[0], vpxi[1], vpxi[2], ci);
+    mxv_c(i2bmat, ci, &frustum[6]);
+
+    vpxi[0] = 1.0;
+    vpxi[1] = -yo;
+    vpxi[2] = -zo;
+    vpack_c(vpxi[0], vpxi[1], vpxi[2], ci);
+    mxv_c(i2bmat, ci, &frustum[9]);
+}
+
+
+/*
+  This function computes the position of the sun in the Itokawa body frame.
+   
+  Input:
+  et:         Ephemeris time
+   
+  Output:
+  sunpos:     The position of the sun in body coordinates
 */
 void getSunPosition(double et, double sunpos[3])
 {
-	double lt;
-	const char* target = "SUN";
-	const char* ref = "IAU_ITOKAWA";
-	const char* abcorr = "LT+S";
-	const char* obs = "ITOKAWA";
-	
-	spkpos_c(target, et, ref, abcorr, obs, sunpos, &lt);
-
+    double lt;
+    const char* target = "SUN";
+    const char* ref = "IAU_ITOKAWA";
+    const char* abcorr = "LT+S";
+    const char* obs = "ITOKAWA";
+    
+    spkpos_c(target, et, ref, abcorr, obs, sunpos, &lt);
+    if (failed_c())
+        return;
+    
     cout.precision(16);
-	cout << "Sun position: " << sunpos[0] << " " << sunpos[1] << " " << sunpos[2] << endl;
+    cout << "Sun position: " << sunpos[0] << " " << sunpos[1] << " " << sunpos[2] << endl;
 }
 
-void saveInfoFile(string filename, string utc, double sunpos[3])
+
+void saveInfoFile(string filename,
+                  string utc,
+                  const double scposb[3],
+                  const double boredir[3],
+                  const double updir[3],
+                  const double frustum[12],
+                  const double sunpos[3])
 {
     ofstream fout(filename.c_str());
 
@@ -136,54 +195,108 @@ void saveInfoFile(string filename, string utc, double sunpos[3])
 
     fout.precision(16);
 
-    fout << "START_TIME      = " << utc << "\n";
-    fout << "STOP_TIME       = " << utc << "\n";
-    fout << "SUN_POSITION_LT = ( ";
+    fout << "START_TIME          = " << utc << "\n";
+    fout << "STOP_TIME           = " << utc << "\n";
+
+    fout << "SPACECRAFT_POSITION = ( ";
+    fout << scientific << scposb[0] << " , ";
+    fout << scientific << scposb[1] << " , ";
+    fout << scientific << scposb[2] << " )\n";
+
+    fout << "BORESIGHT_DIRECTION = ( ";
+    fout << scientific << boredir[0] << " , ";
+    fout << scientific << boredir[1] << " , ";
+    fout << scientific << boredir[2] << " )\n";
+
+    fout << "UP_DIRECTION        = ( ";
+    fout << scientific << updir[0] << " , ";
+    fout << scientific << updir[1] << " , ";
+    fout << scientific << updir[2] << " )\n";
+
+    fout << "FRUSTUM1            = ( ";
+    fout << scientific << frustum[0] << " , ";
+    fout << scientific << frustum[1] << " , ";
+    fout << scientific << frustum[2] << " )\n";
+
+    fout << "FRUSTUM2            = ( ";
+    fout << scientific << frustum[3] << " , ";
+    fout << scientific << frustum[4] << " , ";
+    fout << scientific << frustum[5] << " )\n";
+
+    fout << "FRUSTUM3            = ( ";
+    fout << scientific << frustum[6] << " , ";
+    fout << scientific << frustum[7] << " , ";
+    fout << scientific << frustum[8] << " )\n";
+
+    fout << "FRUSTUM4            = ( ";
+    fout << scientific << frustum[9] << " , ";
+    fout << scientific << frustum[10] << " , ";
+    fout << scientific << frustum[11] << " )\n";
+
+    fout << "SUN_POSITION_LT     = ( ";
     fout << scientific << sunpos[0] << " , ";
     fout << scientific << sunpos[1] << " , ";
     fout << scientific << sunpos[2] << " )\n";
 }
 
+
 /*
 
-  This program creates an info file for each sumfile. For example the
-  file N2516167681.INFO is created for the sumfile N2516167681.SUM.
+  This program creates an info file for each fit file. For example the
+  file N2516167681.INFO is created for the fit file N2516167681.FIT.
 
   This program takes the following input arguments:
 
   1. kernelfiles - a file containing the kernel files
-  2. sum file list - a file containing a list of sum files to process, one per line
+  2. fit file list - a file containing a list of fit files to process, one per line
 
 */
 int main(int argc, char** argv)
 {
     if (argc < 3)
     {
-        cout << "Usage: create_info_files <kernelfiles> <sumfilelist>" << endl;
+        cerr << "Usage: create_info_files <kernelfiles> <fitfilelist>" << endl;
         return 1;
     }
     
     string kernelfiles = argv[1];
-    string sumfilelist = argv[2];
+    string fitfilelist = argv[2];
 
     furnsh_c(kernelfiles.c_str());
 
-    vector<string> sumfiles = loadFileList(sumfilelist);
+    erract_c("SET", 1, (char*)"RETURN");
 
-    for (unsigned int i=0; i<sumfiles.size(); ++i)
+    vector<string> fitfiles = loadFileList(fitfilelist);
+
+    for (unsigned int i=0; i<fitfiles.size(); ++i)
     {
+        cout << "starting " << fitfiles[i] << endl;
+        reset_c();
+        
         string utc;
         double et;
-        
-        loadSumFile(sumfiles[i], utc, et);
-
+        double scposb[3];
+        double boredir[3];
+        double updir[3];
+        double frustum[12];
         double sunPosition[3];
         
-        getSunPosition(et, sunPosition);
+        getEt(fitfiles[i], utc, et);
+        if (failed_c())
+            continue;
 
-        int length = sumfiles[i].size();
-        string infofilename = sumfiles[i].substr(0, length-4) + ".INFO";
-        saveInfoFile(infofilename, utc, sunPosition);
+        getScOrientation(et, scposb, boredir, updir, frustum);
+        if (failed_c())
+            continue;
+
+        getSunPosition(et, sunPosition);
+        if (failed_c())
+            continue;
+
+        int length = fitfiles[i].size();
+        string infofilename = fitfiles[i].substr(0, length-4) + ".INFO";
+        saveInfoFile(infofilename, utc, scposb, boredir, updir, frustum, sunPosition);
+        cout << "finished " << fitfiles[i] << endl;
     }
 
     return 0;
