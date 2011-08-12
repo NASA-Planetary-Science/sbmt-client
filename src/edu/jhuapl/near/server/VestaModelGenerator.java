8030b762395e5d9bb4a84fa9a7f363fb003ce703
Eli Kahn <eliezer.kahn@jhuapl.edu>
2011-08-12 19:37:14 +0000

    updated VestaModelGenerator.java to process latest vesta shape models from Gaskell
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@38530 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- src/edu/jhuapl/near/server/VestaModelGenerator.java
+++ src/edu/jhuapl/near/server/VestaModelGenerator.java
@@ -17,10 +17,11 @@ import edu.jhuapl.near.model.Graticule;
 import edu.jhuapl.near.model.vesta.VestaGraticule;
 import edu.jhuapl.near.util.NativeLibraryLoader;
 
-public class VestaModelGenerator {
-
+public class VestaModelGenerator
+{
     /**
-     * This program converts the Hubble Vesta shape model
+     *
+     * This program converts the Gaskell Vesta shape model
      *
      * In addition this program generates normals and saves it to the vtk file.
      * In addition this program generates a coordinate grid and saves it to a
@@ -31,8 +32,7 @@ public class VestaModelGenerator {
      * - the folder where you want to save the generated files
      *
      * @param args
-     */
-
+    */
     public static void main(String[] args)
     {
         javax.swing.SwingUtilities.invokeLater(new Runnable()
@@ -48,11 +48,12 @@ public class VestaModelGenerator {
         String datadir = args[0];
         String outputdir = args[1];
 
-        String[] pltfiles = {"SHAPE"};
+        String[] pltfiles = {"SHAPE64","SHAPE128","SHAPE256","SHAPE512"};
+        String[] outfiles = {"Vesta_res0","Vesta_res1","Vesta_res2","Vesta_res3"};
 
         try
         {
-            for (int i=0; i<1; ++i)
+            for (int i=0; i<4; ++i)
             {
                 String filename = datadir + "/" + pltfiles[i] + ".PLT";
 
@@ -60,7 +61,7 @@ public class VestaModelGenerator {
                 InputStreamReader isr = new InputStreamReader(fs);
                 BufferedReader in = new BufferedReader(isr);
 
-                String vtkfile = outputdir + "/" + pltfiles[i] + ".vtk";
+                String vtkfile = outputdir + "/" + outfiles[i] + ".vtk";
                 FileWriter fstream = new FileWriter(vtkfile);
                 BufferedWriter out = new BufferedWriter(fstream);
 
@@ -131,18 +132,6 @@ public class VestaModelGenerator {
                 writer.SetFileName(gridfile);
                 writer.SetFileTypeToBinary();
                 writer.Write();
-
-                if (i == 0)
-                {
-                    String coloringfile = datadir + "/VESTA_Elevation.txt";
-                    ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
-                    coloringfile = datadir + "/VESTA_GravitationalAcceleration.txt";
-                    ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
-                    coloringfile = datadir + "/VESTA_GravitationalPotential.txt";
-                    ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
-                    coloringfile = datadir + "/VESTA_Slope.txt";
-                    ModelGeneratorUtil.convertCellDataToPointData(vestaPolyData, coloringfile);
-                }
             }
         }
         catch (IOException e)
@@ -152,6 +141,173 @@ public class VestaModelGenerator {
 
     }
 
+    /*
+     * Similar to previous version but uses VTK's vtkDecimatePro to
+     * do the reductions rather than Gaskell's DUMBER program.
+     * Resulting shape model doesn't look as nice as compared to Gaskell's.
+     *
+    public static void main(String[] args)
+    {
+        javax.swing.SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                // do nothing
+            }
+        });
+
+        NativeLibraryLoader.loadVtkLibrariesLinuxNoX11();
+
+        String shapefile = args[0];
+        String outputdir = args[1];
+
+        try
+        {
+//            for (int i=0; i<1; ++i)
+//            {
+                String filename = shapefile;
+
+                InputStream fs = new FileInputStream(filename);
+                InputStreamReader isr = new InputStreamReader(fs);
+                BufferedReader in = new BufferedReader(isr);
+
+                String vtkfile = outputdir + "/Vesta_res3.vtk";
+                FileWriter fstream = new FileWriter(vtkfile);
+                BufferedWriter out = new BufferedWriter(fstream);
+
+                // Read in the first line which list the number of points and plates
+                String[] vals = in.readLine().trim().split("\\s+");
+
+                int numPoints = Integer.parseInt(vals[0]);
+                int numPlates = Integer.parseInt(vals[1]);
+
+                out.write("# vtk DataFile Version 2.0\n");
+                out.write("VESTA\n");
+                out.write("ASCII\n");
+                out.write("DATASET POLYDATA\n");
+                out.write("POINTS " + numPoints + " float\n");
+
+                for (int j=0; j<numPoints; ++j)
+                {
+                    vals = in.readLine().trim().split("\\s+");
+                    out.write(vals[1] + " " + vals[2] + " " + vals[3] + "\n");
+                }
+
+                out.write("POLYGONS " + numPlates + " " + (numPlates*4) + "\n");
+
+                // Note that the point indices are 1-based in the Gaskell model but vtk
+                // requires 0-based. Therefore subract 1 from each index.
+                for (int j=0; j<numPlates; ++j)
+                {
+                    vals = in.readLine().trim().split("\\s+");
+                    int idx1 = Integer.parseInt(vals[1]) - 1;
+                    int idx2 = Integer.parseInt(vals[2]) - 1;
+                    int idx3 = Integer.parseInt(vals[3]) - 1;
+                    out.write("3 " + idx1 + " " + idx2 + " " + idx3 + "\n");
+                }
+
+                out.close();
+                in.close();
+
+                // Now load the vtk file, generate normals, and save as binary
+                vtkPolyDataReader vestaReader = new vtkPolyDataReader();
+                vestaReader.SetFileName(vtkfile);
+                vestaReader.Update();
+
+                vtkPolyDataNormals normalsFilter = new vtkPolyDataNormals();
+                normalsFilter.SetInputConnection(vestaReader.GetOutputPort());
+                normalsFilter.SetComputeCellNormals(0);
+                normalsFilter.SetComputePointNormals(1);
+                normalsFilter.SplittingOff();
+                normalsFilter.Update();
+                vtkAlgorithmOutput normalsOutput = normalsFilter.GetOutputPort();
+
+                vtkPolyDataWriter writer = new vtkPolyDataWriter();
+                writer.SetInputConnection(normalsOutput);
+                writer.SetFileName(vtkfile);
+                writer.SetFileTypeToBinary();
+                writer.Write();
+
+                // Generate the coordinate grid
+                String gridfile = outputdir + "/coordinate_grid_res" + "3" + ".vtk";
+
+                vtkPolyData vestaPolyData = new vtkPolyData();
+                vestaPolyData.DeepCopy(vestaReader.GetOutput());
+
+                Graticule grid = new VestaGraticule(null);
+                grid.generateGrid(vestaPolyData);
+
+                writer = new vtkPolyDataWriter();
+                writer.SetInput(grid.getGridAsPolyData());
+                writer.SetFileName(gridfile);
+                writer.SetFileTypeToBinary();
+                writer.Write();
+
+                System.out.println(vestaPolyData);
+                System.out.println("Finished converting level 3");
+//            }
+
+
+            double[] reductions = {49152.0/3145728.0, 196608.0/3145728.0, 786432.0/3145728.0};
+
+            // Now decimate the model to produce lower res models
+            for (int i=0; i<3; ++i)
+            {
+                vtkDecimatePro dec = new vtkDecimatePro();
+                //vtkQuadricDecimation dec = new vtkQuadricDecimation();
+                dec.SetInputConnection(normalsOutput);
+                dec.SetTargetReduction(1.0 - reductions[i]);
+                dec.PreserveTopologyOn();
+                dec.SplittingOff();
+                dec.BoundaryVertexDeletionOff();
+                dec.SetMaximumError(Double.MAX_VALUE);
+                dec.AccumulateErrorOn();
+                dec.PreSplitMeshOn();
+                //dec.SetSplitAngle(45);
+                //dec.SetFeatureAngle(75);
+                dec.Update();
+                vtkAlgorithmOutput decOutput = dec.GetOutputPort();
+
+                vtkCleanPolyData cleanFilter = new vtkCleanPolyData();
+                cleanFilter.SetInputConnection(decOutput);
+                cleanFilter.Update();
+                vtkAlgorithmOutput cleanOutput = cleanFilter.GetOutputPort();
+
+                vtkfile = outputdir + "/Vesta_res" + i + ".vtk";
+
+                writer = new vtkPolyDataWriter();
+                writer.SetInputConnection(cleanOutput);
+                writer.SetFileName(vtkfile);
+                writer.SetFileTypeToBinary();
+                writer.Write();
+
+                // Generate the coordinate grid
+                gridfile = outputdir + "/coordinate_grid_res" + i + ".vtk";
+
+                vestaPolyData = new vtkPolyData();
+                vestaPolyData.DeepCopy(dec.GetOutput());
+
+                grid = new VestaGraticule(null);
+                grid.generateGrid(vestaPolyData);
+
+                writer = new vtkPolyDataWriter();
+                writer.SetInput(grid.getGridAsPolyData());
+                writer.SetFileName(gridfile);
+                writer.SetFileTypeToBinary();
+                writer.Write();
+
+                System.out.println(vestaPolyData.GetNumberOfCells());
+                //System.out.println("actual reduction " + dec.GetActualReduction());
+                System.out.println("Finished converting level " + i);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+     */
 
     /*
 
@@ -299,7 +455,7 @@ public class VestaModelGenerator {
                 vtkPolyData vestaPolyData = new vtkPolyData();
                 vestaPolyData.DeepCopy(cleanPolyData.GetOutput());
 
-                Graticule grid = ModelFactory.createVestaGraticuleModel(null);
+                Graticule grid = new VestaGraticule(null);
                 grid.generateGrid(vestaPolyData);
 
                 writer = new vtkPolyDataWriter();
@@ -315,5 +471,5 @@ public class VestaModelGenerator {
         }
 
     }
-*/
+    */
 }
