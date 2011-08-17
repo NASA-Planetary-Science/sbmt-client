7914f911ff6802ed2850eb2a6a35b3a92b870cbc
Eli Kahn <eliezer.kahn@jhuapl.edu>
2011-08-17 20:11:48 +0000

    made generate_fc_sql_database.sh executable
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@38579 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/scripts/generate_amica_backplanes.sh
+++ misc/scripts/generate_amica_backplanes.sh
@@ -9,7 +9,7 @@ AMICA_DIR=/project/nearsdc/data/ITOKAWA/AMICA/images
 AMICA_FIT_FILES=/project/nearsdc/data/internal/allAmicaFiles.txt
 
 # Create a list of all fit files
-find -L $AMICA_DIR -name "*.fit" -type f | sort > $AMICA_FIT_FILES
+#find -L $AMICA_DIR -name "*.fit" -type f | sort > $AMICA_FIT_FILES
 
 
 $JAVA_COMMAND edu.jhuapl.near.server.AmicaBackplanesGenerator $AMICA_FIT_FILES $INERTIAL_FILE
