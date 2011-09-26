dc0cdf096660481daa9993d826dc12058ef51c95
Eli Kahn <eliezer.kahn@jhuapl.edu>
2011-09-26 00:19:09 +0000

    updated paths in scripts and kernels file list for creating info files for FC images
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@39114 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/fc_pds/compile.sh
+++ misc/spice/fc_pds/compile.sh
@@ -2,6 +2,6 @@
 
 # IMPORTANT!! Change the next line to the path of your c-spice
 # installation! Must be at least SPICE version N0064.
-SPICE_DIR=/Users/kahneg1/software/spice/cspice/
+SPICE_DIR=/project/nearsdc/software/spice/cspice/
 
 g++ -Wall create_info_files.cpp -I $SPICE_DIR/include $SPICE_DIR/lib/cspice.a -o create_info_files
--- misc/spice/fc_pds/run.sh
+++ misc/spice/fc_pds/run.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
 # Script for running the create_info_files program
+cd `dirname $0`
+DIR=`pwd`
 
-# For Mac
-rm -rf /Users/kahneg1/projects/near/data/dawn/infofiles
-mkdir -p /Users/kahneg1/projects/near/data/dawn/infofiles
-cd /Users/kahneg1/projects/near/data/dawn/infofiles
-/Volumes/Untitled/near/source/gitsvn/trunk/misc/spice/fc_pds/create_info_files \
-    /Volumes/Untitled/near/source/gitsvn/trunk/misc/spice/fc_pds/kernels.txt \
-    /Users/kahneg1/projects/near/data/dawn/dawn_searchresults.csv
-
-# # For linux
-# rm -rf /home/kahneg1/src/near/data/dawn/infofiles
-# mkdir -p /home/kahneg1/src/near/data/dawn/infofiles
-# cd /home/kahneg1/src/near/data/dawn/infofiles
-# /media/KANGURU2.0/near/source/gitsvn/trunk/misc/spice/fc_pds/create_info_files \
-#     /media/KANGURU2.0/near/source/gitsvn/trunk/misc/spice/fc_pds/kernels.txt \
-#     /home/kahneg1/src/near/data/dawn/dawn_searchresults_1108180220.csv
+rm -rf /project/nearsdc/data-apl/VESTA/FC/infofiles/
+mkdir -p /project/nearsdc/data-apl/VESTA/FC/infofiles
+cd /project/nearsdc/data-apl/VESTA/FC/infofiles
+$DIR/create_info_files $DIR/kernels.txt /project/nearsdc/data-apl/VESTA/FC/dawn_searchresults.csv
