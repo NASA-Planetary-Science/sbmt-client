dcaed04d09b67a702003f7d14d44e2b31035fc1f
Eli Kahn <eliezer.kahn@jhuapl.edu>
2011-11-14 14:47:48 +0000

    updated run.sh script
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@39796 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/lidar_min/run.sh
+++ misc/spice/lidar_min/run.sh
@@ -1,35 +1,42 @@
 #!/bin/sh
 
+export DISPLAY=":20"
+Xvfb $DISPLAY &
+
 cd `dirname $0`
 
-SIZE=`expr 1114386 / 8`
-./lidar-min-icp 0 $SIZE > /tmp/icp-out1 &
+#NUMBER_POINTS=1114386
+#NUMBER_POINTS=1248998
+NUMBER_POINTS=1636893
+
+SIZE=`expr $NUMBER_POINTS / 8`
+./lidar-min-icp 0 $SIZE > /tmp/icp-out-run4/icp-out1 2>&1 &
 
 START=`expr 1 \* $SIZE`
 STOP=`expr 2 \* $SIZE`
-./lidar-min-icp $START $STOP > /tmp/icp-out2 &
+./lidar-min-icp $START $STOP > /tmp/icp-out-run4/icp-out2 2>&1 &
 
 START=`expr 2 \* $SIZE`
 STOP=`expr 3 \* $SIZE`
-./lidar-min-icp $START $STOP > /tmp/icp-out3 &
+./lidar-min-icp $START $STOP > /tmp/icp-out-run4/icp-out3 2>&1 &
 
 START=`expr 3 \* $SIZE`
 STOP=`expr 4 \* $SIZE`
-./lidar-min-icp $START $STOP > /tmp/icp-out4 &
+./lidar-min-icp $START $STOP > /tmp/icp-out-run4/icp-out4 2>&1 &
 
 START=`expr 4 \* $SIZE`
 STOP=`expr 5 \* $SIZE`
-./lidar-min-icp $START $STOP > /tmp/icp-out5 &
+./lidar-min-icp $START $STOP > /tmp/icp-out-run4/icp-out5 2>&1 &
 
 START=`expr 5 \* $SIZE`
 STOP=`expr 6 \* $SIZE`
-./lidar-min-icp $START $STOP > /tmp/icp-out6 &
+./lidar-min-icp $START $STOP > /tmp/icp-out-run4/icp-out6 2>&1 &
 
 START=`expr 6 \* $SIZE`
 STOP=`expr 7 \* $SIZE`
-./lidar-min-icp $START $STOP > /tmp/icp-out7 &
+./lidar-min-icp $START $STOP > /tmp/icp-out-run4/icp-out7 2>&1 &
 
 START=`expr 7 \* $SIZE`
-./lidar-min-icp $START 1114386 > /tmp/icp-out8 &
+./lidar-min-icp $START $NUMBER_POINTS > /tmp/icp-out-run4/icp-out8 2>&1 &
 
 wait
