96f8b322ad41c720d40bf384ad4d619376ffe43e
Eli Kahn <eliezer.kahn@jhuapl.edu>
2011-11-18 18:26:15 +0000

    changed optimize.h header to be compatible with c++
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@39901 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/lidar_min/optimize.h
+++ misc/spice/lidar_min/optimize.h
@@ -1,7 +1,15 @@
 #ifndef __OPTIMIZE_H__
 #define __OPTIMIZE_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* minimizer is both the initial guess and the final returned value */
 void optimizeLbfgs(double (*func)(const double* x), double* minimizer, int numVar);
 
+#ifdef __cplusplus
+}       // closing brace for extern "C"
+#endif
+    
 #endif
