6efb9abd106b6c5b1d8912d6fe5fbf806f4ab57d
Eli Kahn <eliezer.kahn@jhuapl.edu>
2012-01-03 18:37:20 +0000

    changed path to SPICE in compile.sh script
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@40410 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/bds2vertex/compile.sh
+++ misc/spice/bds2vertex/compile.sh
@@ -2,6 +2,6 @@
 
 # IMPORTANT!! Change the next line to the path of your c-spice
 # installation! Must be the alpha_dsk_c version.
-SPICE_DIR=/home/kahneg1/programs/spice/alpha_dsk_c
+SPICE_DIR=/project/nearsdc/software/spice/alpha_dsk_c
 
 gcc bds2vertex.c -Wall -I $SPICE_DIR/include -I $SPICE_DIR/src/dsklib_c $SPICE_DIR/lib/dsklib_c.a $SPICE_DIR/lib/cspice.a -o bds2vertex
