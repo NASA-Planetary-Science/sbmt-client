a3fbbfdde871d8ac0bd7d303e5fdbc664012d65e
Eli Kahn <eliezer.kahn@jhuapl.edu>
2012-01-24 21:13:48 +0000

    increased heap size of programs laucned via shell scripts to 512M
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@40703 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/scripts/setup.sh
+++ misc/scripts/setup.sh
@@ -14,6 +14,6 @@ Xvfb $DISPLAY &
 
 # Setup java command including classpath
 TOP_DIR=`pwd`/../..
-JAVA_COMMAND="java -Djava.library.path=/project/nearsdc/software/vtk_all_platforms/current:$JAVA_HOME/jre/lib/amd64/xawt -Dfile.encoding=UTF-8 -classpath $TOP_DIR/build/classes:$TOP_DIR/lib/vtksb.jar:$TOP_DIR/lib/vtk.jar:$TOP_DIR/lib/fits-1.04.0.jar:$TOP_DIR/lib/jide-oss-2.7.3.jar:$TOP_DIR/lib/joda-time-1.6.jar:$TOP_DIR/lib/mysql-connector-java-5.1.10-bin.jar:$TOP_DIR/lib/commons-io-2.0.jar"
+JAVA_COMMAND="java -Xmx512M -Djava.library.path=/project/nearsdc/software/vtk_all_platforms/current:$JAVA_HOME/jre/lib/amd64/xawt -Dfile.encoding=UTF-8 -classpath $TOP_DIR/build/classes:$TOP_DIR/lib/vtksb.jar:$TOP_DIR/lib/vtk.jar:$TOP_DIR/lib/fits-1.04.0.jar:$TOP_DIR/lib/jide-oss-2.7.3.jar:$TOP_DIR/lib/joda-time-1.6.jar:$TOP_DIR/lib/mysql-connector-java-5.1.10-bin.jar:$TOP_DIR/lib/commons-io-2.0.jar"
 
 echo -e "$JAVA_COMMAND \n"
