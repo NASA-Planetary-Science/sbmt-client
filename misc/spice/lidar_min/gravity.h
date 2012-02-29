ae1b95acecdfebac1a132c7220c18c219ce673ee
Eli Kahn <eliezer.kahn@jhuapl.edu>
2012-02-29 19:00:02 +0000

    added function for computing gravity of body assumed to be a point
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@41601 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- misc/spice/lidar_min/gravity.h
+++ misc/spice/lidar_min/gravity.h
@@ -2,9 +2,25 @@
 #define GRAVITY_H
 
 #include <vtkPolyData.h>
+#include <vtkMath.h>
 
 vtkPolyData* initializeGravity(const char* vtkfile);
 double getGravity(const double fieldPoint[3], double* acc);
+
+inline double getPointGravity(const double fieldPoint[3], double* acc)
+{
+    double rhat[3] = {fieldPoint[0], fieldPoint[1], fieldPoint[2]};
+    double r = vtkMath::Normalize(rhat);
+
+    double r2 = fieldPoint[0]*fieldPoint[0] + fieldPoint[1]*fieldPoint[1] + fieldPoint[2]*fieldPoint[2];
+
+    double potential = 1.0 / r;
+    acc[0] = -rhat[0] / r2;
+    acc[1] = -rhat[1] / r2;
+    acc[2] = -rhat[2] / r2;
+
+    return potential;
+}
 
 
 #endif // GRAVITY_H
