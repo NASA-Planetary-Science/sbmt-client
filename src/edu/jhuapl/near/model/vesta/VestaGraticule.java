836d79b7770df7fb49b91bbacd32daeb08e9908a
Eli Kahn <eliezer.kahn@jhuapl.edu>
2012-08-06 13:15:08 +0000

    removed Vesta_ prefix from graticule files which is how the graticule files of other bodies are named
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@44238 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- src/edu/jhuapl/near/model/vesta/VestaGraticule.java
+++ src/edu/jhuapl/near/model/vesta/VestaGraticule.java
@@ -7,17 +7,17 @@ import edu.jhuapl.near.util.Configuration;
 public class VestaGraticule extends Graticule
 {
     static private final String[] gridFilesPublic = {
-        "/VESTA_PUBLIC/Vesta_coordinate_grid_res0.vtk.gz",
-        "/VESTA_PUBLIC/Vesta_coordinate_grid_res1.vtk.gz",
-        "/VESTA_PUBLIC/Vesta_coordinate_grid_res2.vtk.gz",
-        "/VESTA_PUBLIC/Vesta_coordinate_grid_res3.vtk.gz"
+        "/VESTA_PUBLIC/coordinate_grid_res0.vtk.gz",
+        "/VESTA_PUBLIC/coordinate_grid_res1.vtk.gz",
+        "/VESTA_PUBLIC/coordinate_grid_res2.vtk.gz",
+        "/VESTA_PUBLIC/coordinate_grid_res3.vtk.gz"
     };
 
     static private final String[] gridFilesAplOnly = {
-        "/VESTA/Vesta_coordinate_grid_res0.vtk.gz",
-        "/VESTA/Vesta_coordinate_grid_res1.vtk.gz",
-        "/VESTA/Vesta_coordinate_grid_res2.vtk.gz",
-        "/VESTA/Vesta_coordinate_grid_res3.vtk.gz"
+        "/VESTA/coordinate_grid_res0.vtk.gz",
+        "/VESTA/coordinate_grid_res1.vtk.gz",
+        "/VESTA/coordinate_grid_res2.vtk.gz",
+        "/VESTA/coordinate_grid_res3.vtk.gz"
     };
 
     public VestaGraticule(SmallBodyModel smallBodyModel)
