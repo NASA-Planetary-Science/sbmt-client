cab25d87039b770e6acbcac5030707915f96000b
Eli Kahn <eliezer.kahn@jhuapl.edu>
2013-04-17 16:52:16 +0000

    added coloring data to Phobos
    git-svn-id: http://hardin/svn/sis/projects/near/near_vis/trunk@47756 ca6fedb5-3f05-0410-a19e-b2f146938ed9

--- src/edu/jhuapl/near/model/ModelFactory.java
+++ src/edu/jhuapl/near/model/ModelFactory.java
@@ -312,7 +312,7 @@ public class ModelFactory
                     useAPLServer = true;
 
                 boolean hasColoringData = false;
-                if (LUTETIA.equals(name))
+                if (LUTETIA.equals(name) || PHOBOS.equals(name))
                     hasColoringData = true;
 
                 return new SimpleSmallBody(name, submenu, names, paths, hasColoringData, useAPLServer);
