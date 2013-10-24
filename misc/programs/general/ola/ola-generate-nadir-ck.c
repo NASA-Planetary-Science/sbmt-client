02dab143e5ee12422e4f8dcfe188d6c7af8bdb91
Eli Kahn <eliezer.kahn@jhuapl.edu>
2013-10-24 09:46:45 -0400

    updated file for generating msopck input file needed for generating ck kernel of spacecraft

--- misc/programs/general/ola/ola-generate-nadir-ck.c
+++ misc/programs/general/ola/ola-generate-nadir-ck.c
@@ -3,14 +3,15 @@
 #include <stdint.h>
 #include <string.h>
 #include "SpiceUsr.h"
-#include "SpicePln.h"
 
 
 /************************************************************************
-* This program takes OLA Level 1 science data (for timing info only) and also SPICE data and
-* outputs attitude data of spacecraft in a file which can be used as input to msopck
-* to generate ck kernel of spacecraft. This program is not meant to be delivered to SPOC
-* but is to be used for testing so we have a CK file to work with.
+* This program takes OLA Level 1 science data (for timing info only) and also
+* SPICE data and outputs attitude data of spacecraft in a file which can be
+* used as input to msopck to generate ck kernel of spacecraft. The attitude is
+* based of the ORX_NADIR frame which Scott Turner constructed. This program is
+* not meant to be delivered to SPOC but is to be used for testing purposes so
+* we have a CK file of the spacecraft to work with.
 ************************************************************************/
 
 
@@ -78,66 +79,17 @@ int parseLevel1Record(FILE* fin, struct Level1Record* level1Record)
  */
 int convertLevel1ToMatrix(const struct Level1Record* level1Record, double m[3][3])
 {
-    const char* scname = "ORX";
-    const char* ref = "J2000";
-    const char* abcorr = "NONE";
-    const char* bodyname = "BENNU";
-    double starg[6];
-    double velocityPoint[3];
-    double z[3];
-    double y[3];
-    double x[3];
-    double lt;
-    int found;
-    int id;
+    const char* fromFrame = "J2000";
+    const char* toFrame = "ORX_NADIR";
+    int scid = -64; /* ORX spacecraft ID */
     double et;
-    struct _SpicePlane plane;
 
     /* Convert met to ephemeris time */
-    bodn2c_c(scname, &id, &found);
-    if (found == 0)
-        return 1;
-    scs2e_c(id, level1Record->met, &et);
+    scs2e_c(scid, level1Record->met, &et);
 
-    /* Get spacecraft position */
-    spkezr_c(scname, et, ref, abcorr, bodyname, starg, &lt);
+    pxform_c(fromFrame, toFrame, et, m);
     if (failed_c())
         return 1;
-
-    /* z axis points from spacecraft to center of asteroid */
-    z[0] = -starg[0];
-    z[1] = -starg[1];
-    z[2] = -starg[2];
-    vhat_c(z, z);
-
-    /* y axis is projection of velocity vector into plane normal to z axis */
-
-    /* first create a point that lies along the velocity vector */
-    velocityPoint[0] = starg[0] + starg[3];
-    velocityPoint[1] = starg[1] + starg[4];
-    velocityPoint[2] = starg[2] + starg[5];
-
-    /* then project it into plane normal to z axis */
-    nvp2pl_c(z, starg, &plane);
-    vprjp_c(velocityPoint, &plane, y);
-    y[0] = y[0] - starg[0];
-    y[1] = y[1] - starg[1];
-    y[2] = y[2] - starg[2];
-    /* normalize it */
-    vhat_c(y, y);
-
-    /* find x as cross product of y and z */
-    ucrss_c(y, z, x);
-
-    m[0][0] = x[0];
-    m[0][1] = x[1];
-    m[0][2] = x[2];
-    m[1][0] = y[0];
-    m[1][1] = y[1];
-    m[1][2] = y[2];
-    m[2][0] = z[0];
-    m[2][1] = z[1];
-    m[2][2] = z[2];
 
     return 0;
 }
@@ -148,7 +100,6 @@ int main(int argc, char** argv)
     const char* kernelfiles;
     const char* inputfile;
     const char* outputfile;
-    const char* labelfile;
     FILE* fin;
     FILE* fout;
     struct Level1Record level1Record;
@@ -157,14 +108,13 @@ int main(int argc, char** argv)
 
     if (argc < 4)
     {
-        printf("Usage: ./ola-generate-ck <kernelfiles> <inputfile> <outputfile> <labelfile>\n");;
+        printf("Usage: ./ola-generate-ck <kernelfiles> <inputfile> <outputfile>\n");;
         return 1;
     }
 
     kernelfiles = argv[1];
     inputfile = argv[2];
     outputfile = argv[3];
-    labelfile = argv[4];
 
     /* Load in SPICE metakernel */
     furnsh_c(kernelfiles);
