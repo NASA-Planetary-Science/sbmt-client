598fa7fc0483c8585d54e088646e3565bb74de52
Eli Kahn <eliezer.kahn@jhuapl.edu>
2014-02-06 11:13:54 -0500

    added indexing of Deimos PDS derived images in SQL database

---
+++ misc/server/query/searchdeimosimages_id.php
@@ -0,0 +1,40 @@
+<?
+
+$id=$_POST['id'] + 0;
+$imageSource=$_POST['imageSource'];
+
+$username="nearuser";
+$password="n3ar!usr";
+$database="near";
+$host="sd-mysql.jhuapl.edu:3306";
+
+if (substr($imageSource, 0, 3) == "PDS")
+	$deimosimages="deimosimages_pds";
+else
+	$deimosimages="deimosimages_gaskell";
+
+mysql_connect($host,$username,$password);
+@mysql_select_db($database) or die();
+
+$query = "SELECT filename, starttime FROM $deimosimages ";
+$query .= "WHERE id = " . $id;
+
+$result=mysql_query($query);
+
+$num=mysql_numrows($result);
+
+mysql_close();
+
+$i=0;
+while ($i < $num) 
+{
+	$row       = mysql_fetch_row($result);	
+	$filename  = $row[0];
+	$starttime = $row[1];
+	
+	echo "$filename $starttime\n";
+	
+	$i++;
+}
+
+?>
