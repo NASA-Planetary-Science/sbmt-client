49dfedd52b214dadf47ae5bf80c214faaad596c8
Eli Kahn <eliezer.kahn@jhuapl.edu>
2014-09-30 17:07:11 -0400

    fixed byte swapping issues

--- src/edu/jhuapl/near/util/MathUtil.java
+++ src/edu/jhuapl/near/util/MathUtil.java
@@ -643,11 +643,19 @@ public class MathUtil
     }
 
     // This function is taken from http://www.java2s.com/Code/Java/Language-Basics/Utilityforbyteswappingofalljavadatatypes.htm
-    static public float swap(float value)
+    public static long swap (long value)
     {
-        int intValue = Float.floatToRawIntBits(value);
-        intValue = swap(intValue);
-        return Float.intBitsToFloat(intValue);
+      long b1 = (value >>  0) & 0xff;
+      long b2 = (value >>  8) & 0xff;
+      long b3 = (value >> 16) & 0xff;
+      long b4 = (value >> 24) & 0xff;
+      long b5 = (value >> 32) & 0xff;
+      long b6 = (value >> 40) & 0xff;
+      long b7 = (value >> 48) & 0xff;
+      long b8 = (value >> 56) & 0xff;
+
+      return b1 << 56 | b2 << 48 | b3 << 40 | b4 << 32 |
+             b5 << 24 | b6 << 16 | b7 <<  8 | b8 <<  0;
     }
 
     /**
