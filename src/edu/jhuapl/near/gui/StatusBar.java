46bd21ebe1b98ae14b931d70bb35fde171a04d0e
Eli Kahn <eliezer.kahn@jhuapl.edu>
2015-03-23 09:21:04 -0400

    in status bar made text in left label selectable so can be copied and pasted into another program

--- src/edu/jhuapl/near/gui/StatusBar.java
+++ src/edu/jhuapl/near/gui/StatusBar.java
@@ -3,25 +3,43 @@ package edu.jhuapl.near.gui;
 import java.awt.BorderLayout;
 import java.awt.Font;
 
+import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class StatusBar extends JPanel
 {
-    private JLabel leftLabel;
+    private JEditorPane leftLabel;
     private JLabel rightLabel;
 
     public StatusBar()
     {
         setLayout(new BorderLayout());
-        leftLabel = new JLabel(" ", SwingConstants.LEFT);
+
+        // The following snippet was taken from https://explodingpixels.wordpress.com/2008/10/28/make-jeditorpane-use-the-system-font/
+        // which shows how to make a JEditorPane behave look like a JLabel but still be selectable.
+        leftLabel = new JEditorPane(new HTMLEditorKit().getContentType(), "");
+        leftLabel.setBorder(null);
+        leftLabel.setOpaque(false);
+        leftLabel.setEditable(false);
+        leftLabel.setForeground(UIManager.getColor("Label.foreground"));
+        // add a CSS rule to force body tags to use the default label font
+        // instead of the value in javax.swing.text.html.default.csss
+        Font font = UIManager.getFont("Label.font");
+        String bodyRule = "body { font-family: " + font.getFamily() + "; " +
+                "font-size: " + font.getSize() + "pt; }";
+        ((HTMLDocument)leftLabel.getDocument()).getStyleSheet().addRule(bodyRule);
+
         add(leftLabel, BorderLayout.CENTER);
         rightLabel = new JLabel(" ", SwingConstants.RIGHT);
         add(rightLabel, BorderLayout.EAST);
 
-        Font font = new Font("Monospaced", Font.PLAIN, 13);
+        font = new Font("Monospaced", Font.PLAIN, 13);
         rightLabel.setFont(font);
 
         setBorder(new BevelBorder(BevelBorder.LOWERED));
