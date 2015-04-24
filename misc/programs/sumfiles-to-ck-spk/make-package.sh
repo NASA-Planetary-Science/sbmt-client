ad1a9f12729f9dc0028b896f53d1dd0627e9bb73
Eli Kahn <eliezer.kahn@jhuapl.edu>
2015-04-24 15:34:44 -0400

    update make-package.sh script

--- misc/programs/sumfiles-to-ck-spk/make-package.sh
+++ misc/programs/sumfiles-to-ck-spk/make-package.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Creates tar file of sumfile-to-ck program for distribution.
+# Creates tar file of sumfile-to-ck-spk program for distribution.
 # This script must be run within folder containing this script.
 
-root_dir=/tmp/sumfiles-to-ck
-package_dir=sumfiles-to-ck-2.0.0
+root_dir=/tmp/sumfiles-to-ck-spk
+package_dir=sumfiles-to-ck-spk-3.0.0
 tar_file=$package_dir.tar.gz
 
 mkdir -p $root_dir/$package_dir
 
-cp README.txt compile.sh kernels.txt msopcksetup process_sumfiles.cpp sumfilelist.txt $root_dir/$package_dir
+cp README.txt compile.sh kernels.txt msopcksetup mkspksetup process_sumfiles.cpp sumfilelist.txt $root_dir/$package_dir
 
 cd $root_dir/
 
