e8ec7303c03f70d53b48d945bc4df4fe2c287cf8
Russell Turner <russell.turner@jhuapl.edu>
2015-07-14 14:45:18 -0400

    updated run-pluto.sh to use Howie's precalculated wcs corrected kernel

--- misc/programs/lorri_pds/run-pluto.sh
+++ misc/programs/lorri_pds/run-pluto.sh
@@ -19,8 +19,7 @@ LORRI_IMAGE_FILES=/project/nearsdc/data/internal/allLorriFiles.txt
 LORRI_WCS_IMAGE_FILES=/project/nearsdc/data/internal/allWcsCorrectedLorriFiles.txt
 MSOPCK_SETUP_FILE=/project/nearsdc/data/internal/allLorriMsopckSetup.txt
 MSOPCK_DATA_FILE=/project/nearsdc/data/internal/allLorriMsopckData.txt
-WCS_CORRECTED_KERNEL=./nh_wcs_spice.bc
-SWRI_METAKERNEL=./nh_spice.mk
+LORRI_METAKERNEL=./nh_spice.mk
 WCS_CORRECTED_METAKERNEL=./nh_wcs_spice.mk
 
 # Selects 1x1 images only, will change to include all images
@@ -29,16 +28,27 @@ find -L $LORRI_DIR -name "*sci.fit" -type f | sort > $LORRI_IMAGE_FILES
 # Selects WCS-corrected files
 find -L $LORRI_DIR -name "*pwcs1.fit" -type f | sort > $LORRI_WCS_IMAGE_FILES
 
-$CREATE_WCS_KERNELS -l $LORRI_WCS_IMAGE_FILES -s $MSOPCK_SETUP_FILE -q $MSOPCK_DATA_FILE -m $SWRI_METAKERNEL
+# use Howard Taylors's wcs corrected kernel
+#
+WCS_CORRECTED_CKERNEL=/project/lorri/kernels/nh_lorri_wcs.bc
+#
+# or
+#
+# WCS_CORRECTED_CKERNEL=/project/lorri/kernels/nh_wcs_spice.bc
+#
+# or
+#
+# build corrected kernel ourselves
+#WCS_CORRECTED_CKERNEL=./nh_lorri_wcs.bc
+#$CREATE_WCS_KERNELS -l $LORRI_WCS_IMAGE_FILES -s $MSOPCK_SETUP_FILE -q $MSOPCK_DATA_FILE -m $LORRI_METAKERNEL
+## NOTE: should append a date suffix to the kernel and metakernel files to archive
+#rm -f $WCS_CORRECTED_CKERNEL
+#$MSOPCK $MSOPCK_SETUP_FILE $MSOPCK_DATA_FILE $WCS_CORRECTED_CKERNEL
 
-# NOTE: should append a date suffix to the kernel and metakernel files to archive
-rm -f $WCS_CORRECTED_KERNEL
-$MSOPCK $MSOPCK_SETUP_FILE $MSOPCK_DATA_FILE $WCS_CORRECTED_KERNEL
 
-cp $SWRI_METAKERNEL $WCS_CORRECTED_METAKERNEL
-
+cp $LORRI_METAKERNEL $WCS_CORRECTED_METAKERNEL
 echo '\begindata' >> $WCS_CORRECTED_METAKERNEL
-echo "KERNELS_TO_LOAD += ('$WCS_CORRECTED_KERNEL'"  >> $WCS_CORRECTED_METAKERNEL
+echo "KERNELS_TO_LOAD += ('$WCS_CORRECTED_CKERNEL'"  >> $WCS_CORRECTED_METAKERNEL
 echo  ')'  >> $WCS_CORRECTED_METAKERNEL
 echo '\begintext'  >> $WCS_CORRECTED_METAKERNEL
 
@@ -49,7 +59,7 @@ runCreateInfoFiles() {
 
     rm -f $INFO_DIR/*
     mkdir -p $INFO_DIR
-    CMD="$CREATE_INFO_FILES $1 $SWRI_METAKERNEL $LORRI_IMAGE_FILES $INFO_DIR $STANDARD_FILE_LIST"
+    CMD="$CREATE_INFO_FILES $1 $LORRI_METAKERNEL $LORRI_IMAGE_FILES $INFO_DIR $STANDARD_FILE_LIST"
     $CMD
 
     WCS_CORRECTED_FILE_LIST=/project/nearsdc/data/NEWHORIZONS/$1/IMAGING/wcscorrectedimagelist.txt
