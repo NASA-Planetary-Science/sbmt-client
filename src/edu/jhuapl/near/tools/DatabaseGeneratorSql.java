e49350f536a8b298e3ad5c141399eb089c1b8fc4
nguyel1 <nguyel1@NGUYEL1-WL3.dom1.jhuapl.edu>
2015-10-27 13:47:36 -0400

    Adding CERES to database generator

--- src/edu/jhuapl/near/tools/DatabaseGeneratorSql.java
+++ src/edu/jhuapl/near/tools/DatabaseGeneratorSql.java
@@ -382,6 +382,8 @@ public class DatabaseGeneratorSql
                 "/project/nearsdc/data/GASKELL/ITOKAWA/AMICA/imagelist.txt", "amica"),
         VESTA(SmallBodyConfig.getSmallBodyConfig(ShapeModelBody.VESTA, ShapeModelAuthor.GASKELL),
                 "/project/nearsdc/data/GASKELL/VESTA/FC/uniqFcFiles.txt", "fc"),
+        CERES(SmallBodyConfig.getSmallBodyConfig(ShapeModelBody.CERES, ShapeModelAuthor.GASKELL),
+                "/project/nearsdc/data/GASKELL/CERES/FC/allFcFiles.txt", "fc"),
         DEIMOSEXPERIMENTAL(SmallBodyConfig.getSmallBodyConfig(ShapeModelBody.DEIMOS, ShapeModelAuthor.THOMAS),
                 "/project/nearsdc/data/THOMAS/DEIMOSEXPERIMENTAL/IMAGING/imagelist-fullpath.txt", "deimos"),
         PHOBOS(SmallBodyConfig.getSmallBodyConfig(ShapeModelBody.PHOBOS, ShapeModelAuthor.GASKELL),
