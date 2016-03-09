0708802847f25262145873c4b4cf2c04acacfe71
nguyel1 <nguyel1@NGUYEL1-WL3.dom1.jhuapl.edu>
2016-03-09 13:51:35 -0500

    Show default file name in file chooser for "Save Original FITS Image"

--- src/edu/jhuapl/near/popupmenus/ImagePopupMenu.java
+++ src/edu/jhuapl/near/popupmenus/ImagePopupMenu.java
@@ -18,8 +18,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
-import nom.tam.fits.FitsException;
-
 import vtk.vtkActor;
 import vtk.vtkProp;
 
@@ -43,6 +41,8 @@ import edu.jhuapl.near.model.mvic.MVICQuadJupiterImage;
 import edu.jhuapl.near.util.ColorUtil;
 import edu.jhuapl.near.util.FileCache;
 import edu.jhuapl.near.util.FileUtil;
+
+import nom.tam.fits.FitsException;
 
 
 public class ImagePopupMenu extends PopupMenu
@@ -453,8 +453,9 @@ public class ImagePopupMenu extends PopupMenu
                 PerspectiveImage image = (PerspectiveImage)imageCollection.getImage(imageKey);
                 String path = image.getFitFileFullPath();
                 String extension = path.substring(path.lastIndexOf("."));
+                String imageFileName = new File(path).getName();
 
-                file = CustomFileChooser.showSaveDialog(invoker, "Save FITS image", imageKey.name + extension, "fit");
+                file = CustomFileChooser.showSaveDialog(invoker, "Save FITS image", imageFileName, "fit");
                 if (file != null)
                 {
                     File fitFile = FileCache.getFileFromServer(imageKey.name + extension);
