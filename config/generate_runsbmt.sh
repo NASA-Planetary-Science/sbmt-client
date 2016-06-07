a4095aef4452d5eaa65f664a5a92fae2f0b54115
malldva1 <valerie.mallder@jhuapl.edu>
2016-06-07 14:22:52 -0400

    First working Makefile framework.

---
+++ config/generate_runsbmt.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+#echo $# arguments
+if [ -z "$SBMTROOT" ]
+then
+	echo "ERROR: SBMTROOT is undefined!"
+	exit 1
+fi
+if [ "$#" -lt 2 ]
+then
+	echo "ERROR: must provide output directory and version parameters"
+	exit 1
+fi
+
+output_dir=$1
+version=$2
+
+
+#top_level_dir=`pwd`
+top_level_dir=$SBMTROOT
+
+
+mkdir -p $output_dir/mac64/sbmt/lib
+mkdir -p $output_dir/linux64/sbmt/lib
+mkdir -p $output_dir/win64/sbmt/lib
+
+#echo "top_level_dir=$top_level_dir"
+#echo "output_dir=$output_dir"
+#echo "version=$version"
+
+
+echo -n -e "#!/bin/sh
+echo '**********************************'
+echo 'CLOSING THIS WINDOW WILL QUIT SBMT'
+echo '**********************************'
+DIR=\`dirname \"\$0\"\`
+DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
+export DYLD_LIBRARY_PATH=\"\$DIR/lib/mac64\":\$DYLD_LIBRARY_PATH
+MEMSIZE=\`sysctl hw.memsize | awk '{print int(\$2/1024)}'\`
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/mac64\" -Dcom.apple.mrj.application.apple.menu.about.name=\"Small Body Mapping Tool\" -jar \"\$DIR/lib/near.jar\" \$@
+" > $output_dir/mac64/sbmt/runsbmt
+chmod +x $output_dir/mac64/sbmt/runsbmt
+
+echo -n -e "#!/bin/sh
+DIR=\`dirname \"\$0\"\`
+DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
+export LD_LIBRARY_PATH=\"\$DIR/lib/linux64\":\$LD_LIBRARY_PATH
+MEMSIZE=\`grep MemTotal /proc/meminfo | awk '{print \$2}'\`
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64\" -jar \"\$DIR/lib/near.jar\" \$@
+" > $output_dir/linux64/sbmt/runsbmt
+chmod +x $output_dir/linux64/sbmt/runsbmt
+
+
+if [ "$version" == "internal" ]
+then
+echo -n -e "replace-with-username
+replace-with-password
+" > $output_dir/mac64/sbmt/password.txt
+cp $output_dir/mac64/sbmt/password.txt $output_dir/linux64/sbmt/
+
+echo -n -e "replace-with-username\r
+replace-with-password\r
+" > $output_dir/win64/sbmt/password.txt
+fi
+
---
+++ misc/scripts/nh_spice.mk
@@ -0,0 +1,76 @@
+KPL/MK
+
+New Horizons Meta (FURNSH) Kernel for the LORRI Operations Center
+==============================================================================
+
+This kernel is generated daily.  It contains a list of the correct set
+of SPICE kernels to load using the furnish routine in spice.
+
+This file current as of Wed Jun  1 04:00:01 2016 UTC
+
+Howard Taylor
+JHU/APL
+
+\begindata
+KERNELS_TO_LOAD += ('/project/lorri/kernels/spk/jup260.bsp'
+                    '/project/lorri/kernels/spk/sb-2002jf56-2.bsp'
+                    '/project/lorri/kernels/spk/sb-2014mu69-20150903_s6.bsp'
+                    '/project/lorri/kernels/spk/nh_plu043.bsp'
+                    '/project/lorri/kernels/spk/nh_pred_20070319_20150901_od077.bsp'
+                    '/project/lorri/kernels/spk/nh_pred_20120501_20160913_crit36.bsp'
+                    '/project/lorri/kernels/spk/NavSE_plu047_crit36.bsp'
+                    '/project/lorri/kernels/spk/NavPE_de433_crit36.bsp'
+                    '/project/lorri/kernels/spk/nh_recon_e2j_v1.bsp'
+                    '/project/lorri/kernels/spk/nh_recon_j2sep07_prelimv1.bsp'
+                    '/project/lorri/kernels/spk/nh_recon_pluto_od122_v01.bsp'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels_sci/kernels/ck/nh_scispi_2015_pred.bc'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels_soc/ck/merged_nhpc_2006_v011.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2007_v006.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2008_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2009_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2010_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2011_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2012_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2013_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2014_v001.bc'
+                    '/project/lorri/kernels_soc/ck/merged_nhpc_2015_v038.bc'
+                    '/project/lorri/kernels_soc/ck/2016/merged_nhpc_2016_01_v019.bc'
+                    '/project/lorri/kernels_soc/ck/2016/merged_nhpc_2016_02_v018.bc'
+                    '/project/lorri/kernels_soc/ck/2016/merged_nhpc_2016_03_v006.bc'
+                    '/project/lorri/kernels_soc/ck/2016/merged_nhpc_2016_04_v019.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_122_02.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_123_03.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_124_03.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_125_03.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_126_02.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_127_03.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_128_04.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_129_04.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_131_04.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_134_08.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_137_02.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_139_02.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_140_01.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_143_02.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_145_01.bc'
+                    '/project/lorri/kernels_soc/ck/2016/nhpc_2016_147_01.bc'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels/lsk/naif0011.tls'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels/pck/pck00010.tpc'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels/sclk/new-horizons_1199.tsc'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels/fk/nh_v220.tf'
+                    )
+KERNELS_TO_LOAD += ('/project/lorri/kernels/ik/nh_alice_v200.ti'
+                    '/project/lorri/kernels/ik/nh_lorri_v201.ti'
+                    '/project/lorri/kernels/ik/nh_pepssi_v110.ti'
+                    '/project/lorri/kernels/ik/nh_ralph_v100.ti'
+                    '/project/lorri/kernels/ik/nh_rex_v100.ti'
+                    '/project/lorri/kernels/ik/nh_sdc_v101.ti'
+                    '/project/lorri/kernels/ik/nh_swap_v200.ti'
+                    )
+\begintext
