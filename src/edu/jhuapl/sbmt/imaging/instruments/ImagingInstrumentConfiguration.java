b50a2d86b1f1611e90dd7e550975e55c2e927cac
James Peachey <James.Peachey@jhuapl.edu>
2017-10-11 22:51:22 -0400

    Add configurations for body and model, reshuffle file utilities.

---
+++ src/edu/jhuapl/sbmt/imaging/instruments/ImagingInstrumentConfiguration.java
@@ -0,0 +1,58 @@
+package edu.jhuapl.sbmt.imaging.instruments;
+
+import edu.jhuapl.saavtk.config.Configurable;
+import edu.jhuapl.saavtk.config.Entry;
+import edu.jhuapl.saavtk.config.ExtensibleTypedLookup;
+import edu.jhuapl.saavtk.config.FixedTypedLookup;
+import edu.jhuapl.saavtk.config.Key;
+import edu.jhuapl.saavtk.image.filters.ImageDataFilter;
+import edu.jhuapl.sbmt.client.SpectralMode;
+import edu.jhuapl.sbmt.config.SBMTFileLocator;
+import edu.jhuapl.sbmt.imaging.ImageSource;
+import edu.jhuapl.sbmt.query.QueryBase;
+
+public class ImagingInstrumentConfiguration extends ExtensibleTypedLookup implements Configurable
+{
+    // Required keys.
+    public static final Key<Instrument> INSTRUMENT = Key.of("Instrument");
+    public static final Key<SpectralMode> SPECTRAL_MODE = Key.of("Spectral Mode");
+    public static final Key<QueryBase> QUERY_BASE = Key.of("Search query");
+    public static final Key<ImageSource[]> IMAGE_SOURCE = Key.of("Image source for searches");
+    public static final Key<SBMTFileLocator> FILE_LOCATOR = Key.of("Image file locator");
+
+    // Optional keys.
+    public static final Key<ImageDataFilter> DATA_FILTER = Key.of("Image data filter");
+    public static final Key<String> GALLERY_PATH = Key.of("Gallery path"); // If there is a gallery. Relative to image directory.
+    public static final Key<String> DISPLAY_NAME = Key.of("Display name"); // If different from instrument.toString().
+
+    // Private key to ensure correct fixed configuration is passed to constructor.
+    private static final Key<FixedTypedLookup.Builder> BUILDER_KEY = Key.of("ImagingInstrumentConfiguration builder");
+
+    public static Builder<ImagingInstrumentConfiguration> builder(
+            Instrument instrument,
+            SpectralMode spectralMode,
+            QueryBase queryBase,
+            ImageSource[] imageSource,
+            SBMTFileLocator imageFileLocator)
+    {
+        FixedTypedLookup.Builder fixedBuilder = FixedTypedLookup.builder(BUILDER_KEY);
+        fixedBuilder.put(Entry.of(INSTRUMENT, instrument));
+        fixedBuilder.put(Entry.of(SPECTRAL_MODE, spectralMode));
+        fixedBuilder.put(Entry.of(QUERY_BASE, queryBase));
+        fixedBuilder.put(Entry.of(IMAGE_SOURCE, imageSource));
+        fixedBuilder.put(Entry.of(FILE_LOCATOR, imageFileLocator));
+        return new Builder<ImagingInstrumentConfiguration>(fixedBuilder) {
+            @Override
+            public ImagingInstrumentConfiguration build()
+            {
+                return new ImagingInstrumentConfiguration(getFixedBuilder());
+            }
+        };
+    }
+
+    protected ImagingInstrumentConfiguration(FixedTypedLookup.Builder builder)
+    {
+        super(BUILDER_KEY, builder);
+    }
+
+}
