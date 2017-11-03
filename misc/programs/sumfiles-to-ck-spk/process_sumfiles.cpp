154175d605ba09ee0930d4a40792a080f7219d6e
nguyel1 <nguyel1@NGUYEL1-WL3.dom1.jhuapl.edu>
2017-11-03 09:26:39 -0400

    corrected the original to account for light time and the instrument to spacecraft rotation

--- misc/programs/sumfiles-to-ck-spk/process_sumfiles.cpp
+++ misc/programs/sumfiles-to-ck-spk/process_sumfiles.cpp
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include "SpiceUsr.h"
 
-
 struct TimeMatrix
 {
     std::string utc;
@@ -63,9 +62,13 @@ void myReplace(std::string& str, const std::string& oldStr, const std::string& n
 
 
 void loadSumFile(const std::string& sumfile,
-                 const std::string& asteroidframe,
+		const std::string& instrumentframe,
+		const std::string& spacecraftframe,
+		const std::string& asteroidframe,
+				const std::string& asteroid,
+				const std::string& spacecraft,
                  std::string& utc,
-                 double j2000_to_instrument[3][3],
+                 double j2000_to_sc[3][3],
                  double position[3])
 {
     std::ifstream fin(sumfile.c_str());
@@ -77,6 +80,9 @@ void loadSumFile(const std::string& sumfile,
         double cx[3];
         double cy[3];
         double cz[3];
+
+        double lt, notUsed[6];
+        const char* abcorr = "LT+S";
 
         std::string name;
         std::string dummy;
@@ -148,14 +154,50 @@ void loadSumFile(const std::string& sumfile,
         instrument_to_asteroid[1][2] = cz[1];
         instrument_to_asteroid[2][2] = cz[2];
 
+        /////////////////////////////////////////////////////////////////
+        // The sumfiles provide the instrument_to_asteroid rotation. To
+        // output the spacecraft_to_j2000 rotation to the C-Kernel,
+        // chain the following rotation matrices:
+        //
+        // spacecraft_to_j2000 = asteroid_to_j2000 * instrument_to_asteroid * spacecraft_to_instrument
+        //
+        // where '*' is a matrix multiply (multiplication performed from
+        // right to left).
+        //
+        // The leftmost rotation, asteroid_to_j2000, must be corrected
+        // for light time, since the time in the sumfile is the time at
+        // the spacecraft when the image was exposed, capturing the state
+        // of the body one light time ago.
+        /////////////////////////////////////////////////////////////////
+
+        // Get the time it takes for light to travel from the asteroid to
+        // the spacecraft. Only the returned light time will be used from
+        // this call, so the reference frame does not matter here. Use the
+        // body fixed frame.
+        spkpos_c(spacecraft.c_str(), et, asteroidframe.c_str(), abcorr, asteroid.c_str(), notUsed, &lt);
+
+        // Get the state of the asteroid at the time when it was illuminated,
+        // which is one light time before the image snap time.
         double asteroid_to_j2000[3][3];
-        pxform_c(asteroidframe.c_str(), "J2000", et, asteroid_to_j2000);
+        pxform_c(asteroidframe.c_str(), "J2000", et - lt, asteroid_to_j2000);
 
-        double instrument_to_j2000[3][3];
-        mxm_c(instrument_to_asteroid, asteroid_to_j2000, instrument_to_j2000);
+        // Get the spacecraft to instrument rotation. Time is at the spacecraft,
+        // although time is irrelevant for a fix-mounted instrument.
+        double sc_to_instrument[3][3];
+        pxform_c(spacecraftframe.c_str(), instrumentframe.c_str(), et, sc_to_instrument);
 
-        invert_c(instrument_to_j2000, j2000_to_instrument);
+        // Chain the rotations
+        double sc_to_asteroid[3][3];
+        mxm_c(instrument_to_asteroid, sc_to_instrument, sc_to_asteroid);
 
+        double sc_to_j2000[3][3];
+        mxm_c(asteroid_to_j2000, sc_to_asteroid, sc_to_j2000);
+
+        // Take the inverse. MSOPCK requires this.
+        invert_c(sc_to_j2000, j2000_to_sc);
+
+        // Spacecraft position is in the body-fixed frame. This must be
+        // specified to MKSPK.
         position[0] = scpos[0];
         position[1] = scpos[1];
         position[2] = scpos[2];
@@ -228,15 +270,20 @@ std::ifstream::pos_type filesize(const char* filename)
 
 int main(int argc, char** argv)
 {
-    if (argc < 4)
+    if (argc < 8)
     {
-        std::cout << "Usage: process_sumfiles <kernelfiles> <sumfilelist> <name-of-asteroid-frame>" << std::endl;
+    	// Only works for fix-mounted instruments
+        std::cout << "Usage: process_sumfiles <kernelFiles> <sumfileList> <instrumentFrameName> <spacecraftFrameName> <asteroidFrameName> <asteroidName> <spacecraftName>" << std::endl;
         return 1;
     }
 
     std::string kernelfiles = argv[1];
     std::string sumfilelist = argv[2];
-    std::string asteroidframe = argv[3];
+    std::string instrumentframe = argv[3];
+    std::string spacecraftframe = argv[4];
+    std::string asteroidframe = argv[5];
+    std::string asteroid = argv[6];
+    std::string spacecraft = argv[7];
 
     furnsh_c(kernelfiles.c_str());
 
@@ -253,9 +300,9 @@ int main(int argc, char** argv)
 
         TimeMatrix tm;
 
-        loadSumFile(sumfiles[i], asteroidframe, tm.utc, tm.mat, tm.pos);
+        loadSumFile(sumfiles[i], instrumentframe, spacecraftframe, asteroidframe, asteroid, spacecraft, tm.utc, tm.mat, tm.pos);
 
-        std::cout << sumfiles[i] << " " << tm.utc << std::endl;
+        std::cout << "Processed " << sumfiles[i] << " " << tm.utc << std::endl;
 
         data.push_back(tm);
     }
