b78848190630c9a622ad2a6b4c3393710435d690
James Peachey <James.Peachey@jhuapl.edu>
2017-12-15 13:27:31 -0500

    Check whether access is authorized before attempting to create a model.

--- src/edu/jhuapl/sbmt/client/SbmtModelFactory.java
+++ src/edu/jhuapl/sbmt/client/SbmtModelFactory.java
@@ -11,6 +11,7 @@ import edu.jhuapl.saavtk.model.Model;
 import edu.jhuapl.saavtk.model.ModelNames;
 import edu.jhuapl.saavtk.model.ShapeModelAuthor;
 import edu.jhuapl.saavtk.model.ShapeModelBody;
+import edu.jhuapl.saavtk.util.FileCache;
 import edu.jhuapl.sbmt.model.bennu.Bennu;
 import edu.jhuapl.sbmt.model.bennu.MapCamEarthImage;
 import edu.jhuapl.sbmt.model.bennu.MapCamImage;
@@ -220,6 +221,9 @@ public class SbmtModelFactory
 
     static public SmallBodyModel createSmallBodyModel(SmallBodyViewConfig config)
     {
+        // This method will throw an exception if we cannot access this file.
+        FileCache.getFileInfoFromServer(config.serverPath(""));
+
         ShapeModelBody name = config.body;
         ShapeModelAuthor author = config.author;
 
