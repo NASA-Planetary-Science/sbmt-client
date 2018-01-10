8097b01d0c0a6718f935331965c14529172af232
James Peachey <James.Peachey@jhuapl.edu>
2018-01-09 21:57:07 -0500

    Redirect standard streams during startup so user can view error messages

--- src/edu/jhuapl/sbmt/tools/SbmtRunnable.java
+++ src/edu/jhuapl/sbmt/tools/SbmtRunnable.java
@@ -1,5 +1,10 @@
 package edu.jhuapl.sbmt.tools;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JPopupMenu;
@@ -10,9 +15,10 @@ import vtk.vtkNativeLibrary;
 
 import edu.jhuapl.saavtk.config.ViewConfig;
 import edu.jhuapl.saavtk.gui.MainWindow;
-import edu.jhuapl.saavtk.model.ShapeModelType;
 import edu.jhuapl.saavtk.model.ShapeModelBody;
+import edu.jhuapl.saavtk.model.ShapeModelType;
 import edu.jhuapl.saavtk.util.Configuration;
+import edu.jhuapl.saavtk.util.SafePaths;
 import edu.jhuapl.sbmt.client.SbmtMainWindow;
 import edu.jhuapl.sbmt.client.SmallBodyMappingTool;
 import edu.jhuapl.sbmt.client.SmallBodyMappingTool.Mission;
@@ -20,50 +26,91 @@ import edu.jhuapl.sbmt.client.SmallBodyViewConfig;
 
 public class SbmtRunnable implements Runnable
 {
+    private static final Path OUTPUT_FILE_PATH = SafePaths.get(Configuration.getApplicationDataDir(), "sbmtLogFile.txt");
+    private static final Path SAVED_OUTPUT_FILE_PATH = SafePaths.get(System.getProperty("user.home"), "sbmtLogFile.txt");
+
     private final String[] args;
+    private final PrintStream savedOut;
+    private final PrintStream savedErr;
+
     public SbmtRunnable(String[] args)
     {
         this.args = args;
+        this.savedOut = System.out;
+        this.savedErr = System.err;
     }
 
     public void run()
     {
-        SmallBodyViewConfig.initialize();
-        configureMissionBodies();
+        try (PrintStream outputFile = new PrintStream(Files.newOutputStream(OUTPUT_FILE_PATH)))
+        {
+            redirectStreams(outputFile);
+            SmallBodyViewConfig.initialize();
+            configureMissionBodies();
 
-        // Parse options that come first
-        int i = 0;
-        for (; i < args.length; ++i) {
-            if (args[i].equals("--beta")) {
-                SmallBodyViewConfig.betaMode = true;
-            }else {
-                // We've encountered something that is not an option, must be at the args
-                break;
+            // Parse options that come first
+            int i = 0;
+            for (; i < args.length; ++i) {
+                if (args[i].equals("--beta")) {
+                    SmallBodyViewConfig.betaMode = true;
+                }else {
+                    // We've encountered something that is not an option, must be at the args
+                    break;
+                }
+            }
+
+            // After options comes the args
+            String tempShapeModelPath = null;
+            if (Configuration.isAPLVersion() && args.length - i >= 1){
+                tempShapeModelPath = args[i++];
+            }
+
+            //  NativeLibraryLoader.loadVtkLibraries();
+
+            vtkNativeLibrary.LoadAllNativeLibraries();
+
+            vtkJavaGarbageCollector garbageCollector = new vtkJavaGarbageCollector();
+            //garbageCollector.SetDebug(true);
+            garbageCollector.SetScheduleTime(5, TimeUnit.SECONDS);
+            garbageCollector.SetAutoGarbageCollection(true);
+
+            JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+            ToolTipManager.sharedInstance().setLightWeightPopupEnabled(false);
+            ToolTipManager.sharedInstance().setDismissDelay(600000); // 10 minutes
+
+            MainWindow frame = new SbmtMainWindow(tempShapeModelPath);
+//        MainWindow frame = new MainWindow("data/Torso.stl");
+            frame.setVisible(true);
+        }
+        catch (@SuppressWarnings("unused") Exception e)
+        {
+            // Something went tragically wrong, so move the output file to a more
+            // prominent location.
+            try
+            {
+                Files.move(OUTPUT_FILE_PATH, SAVED_OUTPUT_FILE_PATH, StandardCopyOption.REPLACE_EXISTING);
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
             }
         }
-
-        // After options comes the args
-        String tempShapeModelPath = null;
-        if (Configuration.isAPLVersion() && args.length - i >= 1){
-            tempShapeModelPath = args[i++];
+        finally
+        {
+            restoreStreams();
         }
+    }
 
-      //  NativeLibraryLoader.loadVtkLibraries();
+    protected void redirectStreams(final PrintStream outputFile)
+    {
+        System.setOut(outputFile);
+        System.setErr(outputFile);
+    }
 
-        vtkNativeLibrary.LoadAllNativeLibraries();
-
-        vtkJavaGarbageCollector garbageCollector = new vtkJavaGarbageCollector();
-        //garbageCollector.SetDebug(true);
-        garbageCollector.SetScheduleTime(5, TimeUnit.SECONDS);
-        garbageCollector.SetAutoGarbageCollection(true);
-
-        JPopupMenu.setDefaultLightWeightPopupEnabled(false);
-        ToolTipManager.sharedInstance().setLightWeightPopupEnabled(false);
-        ToolTipManager.sharedInstance().setDismissDelay(600000); // 10 minutes
-
-        MainWindow frame = new SbmtMainWindow(tempShapeModelPath);
-//        MainWindow frame = new MainWindow("data/Torso.stl");
-        frame.setVisible(true);
+    protected void restoreStreams()
+    {
+        System.setErr(savedErr);
+        System.setOut(savedOut);
     }
 
     protected void configureMissionBodies()
