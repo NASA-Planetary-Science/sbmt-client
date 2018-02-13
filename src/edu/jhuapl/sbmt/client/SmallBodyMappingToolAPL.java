8ebdf60b7e1b3516e4257e012ae024eb505659a6
James Peachey <James.Peachey@jhuapl.edu>
2018-02-13 14:03:27 -0500

    On MacOS, set the name in the apple menu to be "Small Body Mapping Tool"

--- src/edu/jhuapl/sbmt/client/SmallBodyMappingToolAPL.java
+++ src/edu/jhuapl/sbmt/client/SmallBodyMappingToolAPL.java
@@ -19,6 +19,17 @@ public class SmallBodyMappingToolAPL
 {
     public static void main(String[] args)
     {
+        String opSysName = System.getProperty("os.name").toLowerCase();
+        if (opSysName.contains("mac"))
+        {
+            // to set the name of the app in the Mac App menu:
+            System.setProperty("apple.awt.application.name", "Small Body Mapping Tool");
+            //to show the menu bar at the top of the screen:
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            // to show a more mac-like file dialog box
+            System.setProperty("apple.awt.fileDialogForDirectories", "true");
+        }
+
         if (SbmtMultiMissionTool.getOption(args, "--debug") != null)
         {
             Debug.setEnabled(true);
