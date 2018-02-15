255c630681a28c84f1408c759ab0f164d469e491
James Peachey <James.Peachey@jhuapl.edu>
2018-02-15 08:23:19 -0500

    Don't bother with mission command line. Don't make blank password.txt.

--- config/generate_runsbmt.sh
+++ config/generate_runsbmt.sh
@@ -15,7 +15,6 @@ fi
 
 output_dir=$1
 version=$2
-mission=$3
 
 #top_level_dir=`pwd`
 top_level_dir=$SBMTROOT
@@ -35,7 +34,7 @@ DIR=\`dirname \"\$0\"\`
 DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
 export DYLD_LIBRARY_PATH=\"\$DIR/lib/mac64\":\$DYLD_LIBRARY_PATH
 MEMSIZE=\`sysctl hw.memsize | awk '{print int(\$2/1024)}'\`
-\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/mac64\" -Dcom.apple.mrj.application.apple.menu.about.name=\"Small Body Mapping Tool\" -Dedu.jhuapl.sbmt.mission=$mission -jar \"\$DIR/lib/near.jar\" \$@ &
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/mac64\" -jar \"\$DIR/lib/near.jar\" \$@ &
 " > $output_dir/mac64/sbmt/runsbmt
 chmod +x $output_dir/mac64/sbmt/runsbmt
 
@@ -44,20 +43,6 @@ DIR=\`dirname \"\$0\"\`
 DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
 export LD_LIBRARY_PATH=\"\$DIR/lib/linux64\":\$LD_LIBRARY_PATH
 MEMSIZE=\`grep MemTotal /proc/meminfo | awk '{print \$2}'\`
-\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64\" -Dedu.jhuapl.sbmt.mission=$mission  -jar \"\$DIR/lib/near.jar\" \$@ &
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64\" -jar \"\$DIR/lib/near.jar\" \$@ &
 " > $output_dir/linux64/sbmt/runsbmt
 chmod +x $output_dir/linux64/sbmt/runsbmt
-
-
-if [ "$version" == "internal" ]
-then
-echo -n -e "replace-with-username
-replace-with-password
-" > $output_dir/mac64/sbmt/password.txt
-cp $output_dir/mac64/sbmt/password.txt $output_dir/linux64/sbmt/
-
-echo -n -e "replace-with-username\r
-replace-with-password\r
-" > $output_dir/win64/sbmt/password.txt
-fi
-
