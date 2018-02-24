6eb03fbb4f1cf7c5c16aa68f795de3a4523a51dc
James Peachey <James.Peachey@jhuapl.edu>
2018-02-24 12:16:16 -0500

    Handle a Camera property change by offsetting all the images.

--- src/edu/jhuapl/sbmt/client/SbmtModelManager.java
+++ src/edu/jhuapl/sbmt/client/SbmtModelManager.java
@@ -1,9 +1,15 @@
 package edu.jhuapl.sbmt.client;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import edu.jhuapl.saavtk.gui.render.camera.CameraProperties;
 import edu.jhuapl.saavtk.model.AbstractModelManager;
+import edu.jhuapl.saavtk.model.Model;
+import edu.jhuapl.saavtk.model.ModelNames;
 import edu.jhuapl.saavtk.model.PolyhedralModel;
+import edu.jhuapl.sbmt.model.image.Image;
+import edu.jhuapl.sbmt.model.image.ImageCollection;
 
 public class SbmtModelManager extends AbstractModelManager implements PropertyChangeListener
 {
@@ -15,6 +21,34 @@ public class SbmtModelManager extends AbstractModelManager implements PropertyCh
     public SmallBodyModel getPolyhedralModel()
     {
         return (SmallBodyModel)super.getPolyhedralModel();
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt)
+    {
+        // This is a hack. Ideally, the Model interface and implementors would
+        // use a generic update mechanism, but currently there is no general framework
+        // for specific types of models (in this case Images) to get specific
+        // information from other abstractions (in this case, Cameras).
+        if (CameraProperties.CAMERA_DISTANCE.equals(evt.getPropertyName()))
+        {
+            Model model = getModel(ModelNames.IMAGES);
+            if (model instanceof ImageCollection)
+            {
+                ImageCollection collection = (ImageCollection) model;
+                for (Image image : collection.getImages())
+                {
+                    double offset = image.getOffset();
+                    offset = Math.max(0.001 * (double) evt.getNewValue(), image.getOffset());
+                    Image.applyOffset(image, offset);
+                }
+            }
+        }
+        else
+        {
+            super.propertyChange(evt);
+        }
+
     }
 
 }
