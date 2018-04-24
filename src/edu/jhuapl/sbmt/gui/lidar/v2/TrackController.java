a8c51f4c42feb8236707f386043e5827c8e05d6b
Josh Steele <josh.steele@jhuapl.edu>
2018-04-24 13:18:00 -0400

    Extension of the LidarSearchController that doesn't include the search panel

---
+++ src/edu/jhuapl/sbmt/gui/lidar/v2/TrackController.java
@@ -0,0 +1,33 @@
+package edu.jhuapl.sbmt.gui.lidar.v2;
+
+import edu.jhuapl.saavtk.gui.render.Renderer;
+import edu.jhuapl.saavtk.model.ModelManager;
+import edu.jhuapl.saavtk.model.ModelNames;
+import edu.jhuapl.saavtk.pick.PickManager;
+import edu.jhuapl.sbmt.client.BodyViewConfig;
+
+/**
+ * Panel used for showing custom tracks. It is essentially the LidarSearchPanel but with the search
+ * controls removed.
+ */
+public class TrackController extends LidarSearchController
+{
+
+    public TrackController(BodyViewConfig polyhedralModelConfig,
+            ModelManager modelManager, PickManager pickManager,
+            Renderer renderer)
+    {
+        super(polyhedralModelConfig, modelManager, pickManager, renderer);
+        hideSearchControls();
+
+//        if (polyhedralModelConfig.getUniqueName().equals("Gaskell/Bennu (V3 Image)"))
+//            fileTypeComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "OLA Level 2", "Text" }));
+
+    }
+
+    @Override
+    protected ModelNames getLidarModelName()
+    {
+        return ModelNames.TRACKS;
+    }
+}
