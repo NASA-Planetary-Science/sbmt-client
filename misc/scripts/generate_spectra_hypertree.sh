6a9d6390138125219f7d086c7552438ccf7e3821
Colleen <colleen.oshea@jhuapl.edu>
2018-08-14 16:29:30 -0400

    updated generation script

--- misc/scripts/generate_spectra_hypertree.sh
+++ misc/scripts/generate_spectra_hypertree.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 
 # generate the bounding boxes and write to bounding box file
-./run_java_program.sh edu.jhuapl.sbmt.model.spectrum.hypertree.SpectrumBoundsCalculator project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2/otes_l2_bounds.txt /earth/osirisrex/otes/l2/infofiles-corrected OTES > calculateBounds.log
+./run_java_program.sh edu.jhuapl.sbmt.model.spectrum.hypertree.SpectrumBoundsCalculator /project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2/otes_l2_bounds.txt /earth/osirisrex/otes/l2 OTES > calculateBounds.log
 
 # generate the hypertree
 ./run_java_program.sh edu.jhuapl.sbmt.boundedobject.hyperoctree.BoundedObjectHyperTreeGenerator /earth/osirisrex/otes/l2/otes_l2_bounds.txt /earth/osirisrex/otes/l2/hypertree/ 25 -6378.13720703125 6378.13720703125 -6378.13720703125 6378.13720703125 -6356.75244140625 6356.75244140625 > generateTree.log
