87d14524655c604766c9714d11d06452713484f0
Colleen <colleen.oshea@jhuapl.edu>
2018-08-17 11:54:01 -0400

    add max number of spectra per leaf is input

--- misc/scripts/generate_spectra_hypertree.sh
+++ misc/scripts/generate_spectra_hypertree.sh
@@ -5,12 +5,13 @@ cd `dirname $0`
 INSTRUMENT=$1
 TYPE=$2
 BODY=$3
+MAX=$4
 
 # generate the bounding boxes and write to bounding box file
 ./run_java_program.sh edu.jhuapl.sbmt.model.spectrum.hypertree.SpectrumBoundsCalculator $INSTRUMENT $TYPE $BODY > calculateBounds.log
 
 # generate the hypertree
-./run_java_program.sh edu.jhuapl.sbmt.model.boundedobject.hyperoctree.BoundedObjectHyperTreeGenerator $INSTRUMENT $TYPE $BODY 25 TEST > generateTree.log
+./run_java_program.sh edu.jhuapl.sbmt.model.boundedobject.hyperoctree.BoundedObjectHyperTreeGenerator $INSTRUMENT $TYPE $BODY $MAX TEST > generateTree.log
 
 #condense the hypertree into a bounds file .spectra
 ./run_java_program.sh edu.jhuapl.sbmt.model.spectrum.hypertree.SpectrumHypertreeCondenser > condenseTree.log
