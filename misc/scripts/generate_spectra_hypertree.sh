08385629c10721ad297de1ed19e5de04a9c37b28
Colleen <colleen.oshea@jhuapl.edu>
2018-08-17 10:22:18 -0400

    working on generic input and filepaths

--- misc/scripts/generate_spectra_hypertree.sh
+++ misc/scripts/generate_spectra_hypertree.sh
@@ -2,11 +2,19 @@
 
 cd `dirname $0`
 
+INSTRUMENT=$1
+TYPE=$2
+BODY=$3
+
 # generate the bounding boxes and write to bounding box file
-./run_java_program.sh edu.jhuapl.sbmt.model.spectrum.hypertree.SpectrumBoundsCalculator /project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2/otes_l2_bounds.txt /project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2 OTES > calculateBounds.log
+./run_java_program.sh edu.jhuapl.sbmt.model.spectrum.hypertree.SpectrumBoundsCalculator $INSTRUMENT $TYPE $BODY > calculateBounds.log
 
 # generate the hypertree
-./run_java_program.sh edu.jhuapl.sbmt.model.boundedobject.hyperoctree.BoundedObjectHyperTreeGenerator /project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2/otes_l2_bounds.txt /project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2/hypertree/ 25 TEST -6378.13720703125 6378.13720703125 -6378.13720703125 6378.13720703125 -6356.75244140625 6356.75244140625 > generateTree.log
+./run_java_program.sh edu.jhuapl.sbmt.model.boundedobject.hyperoctree.BoundedObjectHyperTreeGenerator $INSTRUMENT $TYPE $BODY 25 TEST > generateTree.log
 
 #condense the hypertree into a bounds file .spectra
 ./run_java_program.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeCondensor /project/sbmt2/sbmt/data/servers/multi-mission/test/earth/osirisrex/otes/l2/hypertree/ > condenseTree.log
+
+# remove the temp files and copy the tree to actual paths
+rm -f bounds_*.bounds
+cp temp_hypertree/ ____new location____
