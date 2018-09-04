556a69ce155bb9d03763a53cafa67d928bd0dac4
James Peachey <James.Peachey@jhuapl.edu>
2018-09-04 13:54:45 -0400

    Improve error handling of code that checks for db tables.

--- misc/server/query/internal/tableexists.php
+++ misc/server/query/internal/tableexists.php
@@ -1,27 +1,41 @@
 <?php
-    
+
+    if(!isset($_POST['tableName']))
+    {
+        die ("Error calling tableexists.php: tableName is not defined");
+    }
+
     // Get posted data that we know the keys to
-    $imagesDatabase=$_POST['imagesDatabase'];
-    
+    $databaseTable=$_POST['tableName'];
+
     $username="nearuser";
     $password="n3ar!usr";
     $database="near";
     $host="sd-mysql.jhuapl.edu:3306";
-    
+
     $link = mysql_connect($host,$username,$password);
     if (!$link)
     {
-        die('Could not connect: ' . mysql_error());
+        die('Unable to connect to database server: ' . mysql_error());
     }
-    @mysql_select_db($database) or die("died!");
-    
-    $val = mysql_query("SELECT 1 FROM $imagesDatabase LIMIT 1");
-    
+
+    $selected = mysql_select_db($database);
+    if(!$selected)
+    {
+        die ("Unable to select database $database: " .  mysql_error());
+    }
+
+    $val = mysql_query("SELECT 1 FROM $databaseTable LIMIT 1");
+
     if(!$val)
     {
         echo "false";
     }
-    
-    ?>
+    else
+    {
+        echo "true";
+    }
+
+?>
 
 
