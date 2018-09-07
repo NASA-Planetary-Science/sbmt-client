0f5e84433289707af6a40cd39945685f93679b8b
Josh Steele <josh.steele@jhuapl.edu>
2018-09-07 12:26:51 -0400

    HOTFIX: Now handles lidar browse access and display in a more graceful manner

--- src/edu/jhuapl/sbmt/gui/lidar/Hayabusa2LaserLidarBrowsePanel.java
+++ src/edu/jhuapl/sbmt/gui/lidar/Hayabusa2LaserLidarBrowsePanel.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 import edu.jhuapl.saavtk.model.ModelManager;
 import edu.jhuapl.saavtk.util.FileCache;
+import edu.jhuapl.saavtk.util.FileCache.UnauthorizedAccessException;
 import edu.jhuapl.sbmt.client.SmallBodyViewConfig;
 import edu.jhuapl.sbmt.model.lidar.LidarBrowseDataCollection.LidarDataFileSpec;
 import edu.jhuapl.sbmt.util.TimeUtil;
@@ -27,37 +28,48 @@ public class Hayabusa2LaserLidarBrowsePanel extends LidarBrowsePanel
     {
         lidarResultListModel.clear();
 
-        File listFile=FileCache.getFileFromServer(browseFileList);
         try
         {
-            Scanner scanner=new Scanner(new FileInputStream(listFile));
-            while (scanner.hasNext())
+            isDataGettable();
+            File listFile=FileCache.getFileFromServer(browseFileList);
+            try
             {
-                String filename=scanner.next();
-                double startTime=Double.valueOf(scanner.next());
-                double endTime=Double.valueOf(scanner.next());
-                LidarDataFileSpec spec=new LidarDataFileSpec();
-                spec.path=filename;
-                spec.name=Paths.get(filename).getFileName().toString();
-                spec.comment=TimeUtil.et2str(startTime)+" - "+TimeUtil.et2str(endTime);
-                //System.out.println(spec.comment);
-                lidarResultListModel.addElement(spec);
-                //System.out.println(filename);
+                Scanner scanner=new Scanner(new FileInputStream(listFile));
+                while (scanner.hasNext())
+                {
+                    String filename=scanner.next();
+                    double startTime=Double.valueOf(scanner.next());
+                    double endTime=Double.valueOf(scanner.next());
+                    LidarDataFileSpec spec=new LidarDataFileSpec();
+                    spec.path=filename;
+                    spec.name=Paths.get(filename).getFileName().toString();
+                    spec.comment=TimeUtil.et2str(startTime)+" - "+TimeUtil.et2str(endTime);
+                    //System.out.println(spec.comment);
+                    lidarResultListModel.addElement(spec);
+                    //System.out.println(filename);
+                }
+                scanner.close();
             }
-            scanner.close();
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            //Create the list and put it in a scroll pane.
+            //resultList = new JList(lidarResultListModel);
+            //resultList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            //resultList.addListSelectionListener(this);
+
+            resultsLabel.setText("Available Files: "+datasourceName);
         }
-        catch (IOException e)
+        catch (UnauthorizedAccessException e)
         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            resultsLabel.setText("No Results Available: Access Not Authorized");
         }
 
-        //Create the list and put it in a scroll pane.
-        //resultList = new JList(lidarResultListModel);
-        //resultList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        //resultList.addListSelectionListener(this);
 
-        resultsLabel.setText("Available Files: "+datasourceName);
+
 
     }
 
