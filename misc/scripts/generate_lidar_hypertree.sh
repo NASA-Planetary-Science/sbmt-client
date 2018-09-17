1bd4d938a8f391e17cebb4712a6f90cb4028aef7
Colleen <colleen.oshea@jhuapl.edu>
2018-09-17 12:09:49 -0400

    fix headless load and script

--- misc/scripts/generate_lidar_hypertree.sh
+++ misc/scripts/generate_lidar_hypertree.sh
@@ -7,6 +7,8 @@ TYPE=$2
 BODY=$3
 MAX=$4
 
+mkdir test_hypertree/
+rm -rf test_hypertee/*
 # generate the hypertree
 ./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator /project/sbmt2/sbmt/data/bodies/ryugu/shared/lidar/browse/dataDirList.txt test_hypertree/ 1 32 -1 LASER
 
