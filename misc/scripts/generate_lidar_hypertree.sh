f8b8bacac9dd836005f5e4234058f78d119fbba3
Colleen <colleen.oshea@jhuapl.edu>
2018-10-04 16:13:27 -0400

    woops, fix script again and change smallbodyviewconfig path

--- misc/scripts/generate_lidar_hypertree.sh
+++ misc/scripts/generate_lidar_hypertree.sh
@@ -2,14 +2,15 @@
 
 cd `dirname $0`
 
-TREEDIR=/project/sbmt2/sbmt/data/servers/multi-mission/test/data/ryugu/shared/laser/search/test_hypertree
+TREEDIR=/project/sbmt2/sbmt/data/servers/multi-mission/test/ryugu/shared/laser/search/hypertree
 
-mkdir TREEDIR
-rm -rf TREEDIR/*
+mkdir $TREEDIR
+rm -rf $TREEDIR/*
 # generate the hypertree
 ./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator /project/sbmt2/sbmt/data/bodies/ryugu/shared/lidar/browse/dataDirList.txt $TREEDIR 1 32 -1 LASER
 
 #condense the hypertree into a bounds file .spectra
-./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.laser.Hayabusa2LaserFSHyperTreeCondenser TREEDIR
+./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.laser.Hayabusa2LaserFSHyperTreeCondenser $TREEDIR
+
 
 
