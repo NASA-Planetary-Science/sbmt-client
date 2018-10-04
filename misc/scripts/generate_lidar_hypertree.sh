d3fdbe69c4aad19d4add9ed774bf1f741f705c3e
Colleen <colleen.oshea@jhuapl.edu>
2018-10-04 15:56:33 -0400

    update script

--- misc/scripts/generate_lidar_hypertree.sh
+++ misc/scripts/generate_lidar_hypertree.sh
@@ -2,15 +2,14 @@
 
 cd `dirname $0`
 
-INSTRUMENT=$1
-TYPE=$2
-BODY=$3
-MAX=$4
+TREEDIR=/project/sbmt2/sbmt/data/servers/multi-mission/test/data/ryugu/shared/laser/search/test_hypertree
 
-mkdir test_hypertree/
-rm -rf test_hypertee/*
+mkdir TREEDIR
+rm -rf TREEDIR/*
 # generate the hypertree
-./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator /project/sbmt2/sbmt/data/bodies/ryugu/shared/lidar/browse/dataDirList.txt test_hypertree/ 1 32 -1 LASER
+./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator /project/sbmt2/sbmt/data/bodies/ryugu/shared/lidar/browse/dataDirList.txt $TREEDIR 1 32 -1 LASER
 
 #condense the hypertree into a bounds file .spectra
-./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.laser.Hayabusa2LaserFSHyperTreeCondenser test_hypertree/
+./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.laser.Hayabusa2LaserFSHyperTreeCondenser TREEDIR
+
+
