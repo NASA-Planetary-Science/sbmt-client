fc78ad333605ce017f33de36aa91a29a24e5bb23
James Peachey <James.Peachey@jhuapl.edu>
2018-11-19 14:49:59 -0500

    Point at APL database URL by default.

--- src/edu/jhuapl/sbmt/tools/DatabaseGeneratorSql.java
+++ src/edu/jhuapl/sbmt/tools/DatabaseGeneratorSql.java
@@ -409,9 +409,11 @@ public class DatabaseGeneratorSql
         String dburl = null;
         if (SbmtMultiMissionTool.getMission() == Mission.HAYABUSA2_STAGE)
             dburl = "hyb2sbmt.jhuapl.edu";
-        if (SbmtMultiMissionTool.getMission() == Mission.HAYABUSA2_DEPLOY)
+        else if (SbmtMultiMissionTool.getMission() == Mission.HAYABUSA2_DEPLOY)
             dburl = "hyb2sbmt.u-aizu.ac.jp";
-        else if (SbmtMultiMissionTool.getMission() == Mission.HAYABUSA2_DEV)
+        else if (SbmtMultiMissionTool.getMission() == Mission.OSIRIS_REX_DEPLOY)
+            throw new AssertionError("NEED TO SET UP THE LOCATION OF THE OREX DEPLOYED DATABASE");
+        else
             dburl = "sd-mysql.jhuapl.edu";
 
         try
