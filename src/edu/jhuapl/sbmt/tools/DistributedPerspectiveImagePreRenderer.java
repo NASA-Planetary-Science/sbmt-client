3dbab3d89a893e82c617ed684aea761d2c782b42
Josh Steele <josh.steele@jhuapl.edu>
2019-01-11 16:24:17 -0500

    updates to run the qsub from within java, so it can be split up into smaller patches

--- src/edu/jhuapl/sbmt/tools/DistributedPerspectiveImagePreRenderer.java
+++ src/edu/jhuapl/sbmt/tools/DistributedPerspectiveImagePreRenderer.java
@@ -1,12 +1,10 @@
 package edu.jhuapl.sbmt.tools;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.io.FilenameUtils;
 import org.ggf.drmaa.DrmaaException;
 import org.ggf.drmaa.JobTemplate;
 import org.ggf.drmaa.Session;
@@ -19,106 +17,83 @@ import edu.jhuapl.sbmt.model.image.ImageSource;
 public class DistributedPerspectiveImagePreRenderer
 {
 
-    public DistributedPerspectiveImagePreRenderer(String inputDir, String outputPath, ImageSource pointingSource, int instrumentIndex, ShapeModelBody body, ShapeModelType type)
+    public DistributedPerspectiveImagePreRenderer(String inputDir, String outputPath, ImageSource pointingSource, int instrumentIndex, ShapeModelBody body, ShapeModelType type, boolean reprocess)
     {
-        // TODO Auto-generated constructor stub
+    	 SessionFactory factory = SessionFactory.getFactory();
+         Session session = factory.getSession();
+         {
+             try
+             {
+                 session.init("");
+                 JobTemplate jt = session.createJobTemplate();
+//                 String dateString = new SimpleDateFormat("yyyyMMdd").format(new Date());
+                 String outputLogPath = "/clusterOutputFiles/";
+                 File outputDir = new File(System.getProperty("user.home")+outputLogPath);
+                 if (!outputDir.exists()) outputDir.mkdirs();
+                 jt.setOutputPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
+                 jt.setErrorPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
 
-//
-//
-//        SessionFactory factory = SessionFactory.getFactory();
-//        Session session = factory.getSession();
-//        {
-//            try
-//            {
-//                session.init("");
-//                JobTemplate jt = session.createJobTemplate();
-//                String dateString = new SimpleDateFormat("yyyyMMdd").format(new Date());
-//                File outputDir = new File(outputPath);
-//                if (!outputDir.exists()) outputDir.mkdirs();
-//                jt.setOutputPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
-//                jt.setErrorPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
-//
-////                for (int i=-1; i<20; i++)
-//////                    if ((i!=6) && (i!=7))
-////                        runReport(i, jt, session);
-////                session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
-//////                runReport(6, jt, session);
-//////                runReport(7, jt, session);
-//////                session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
-////                jt.setRemoteCommand(scriptDir + "/report2.sh");           //script directory
-//                List<String> list = new ArrayList<String>();
-//                list.add(e)
-////                list.add(jarDir);                                      //jar Directory
-////                list.add(workDir);                                      //workDirectory
-////                list.add(runName);
-////                list.add(resName);                                      //resName
-//                jt.setArgs(list);
-//                String id = session.runJob(jt);
-//                System.out.println("Your job has been submitted with id " + id + " for summary reports");
-//                session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
-//                session.deleteJobTemplate(jt);
-//                session.exit();
-//
-//            }
-//            catch (DrmaaException e)
-//            {
-//                System.out.println("Error: "  + e.getMessage());
-//                session.exit();
-//            }
-//        }
+                 //imageFileName GASKELL RQ36 ALTWG-SPC-v20181109b 0 $baseDir/support $reprocess
+                 jt.setRemoteCommand("/homes/workspace2/preRenderImage.sh");
+                 List<String> argList = new ArrayList<String>();
+                 File[] fileList = new File(inputDir).listFiles();
+                 int i=0;
+                 for (; i<fileList.length;)
+                 {
+                	 int nextBatchLength = Math.min(100, fileList.length - i);
+	                 for (int j=0; j<100; j++)
+	                 {
+	                     argList.add(fileList[i].getAbsolutePath());
+	                     argList.add(pointingSource.toString());
+	                     argList.add(body.toString());
+	                     argList.add(type.toString());
+	                     argList.add("" + instrumentIndex);
+	                     argList.add(outputPath);
+	                     argList.add(""+reprocess);
+	                     jt.setArgs(argList);
+	                     String id = session.runJob(jt);
+	                     System.out.println("Your job has been submitted with id " + id + " for image pre-rendering");
+	                     argList.clear();
+	                 }
+	                 //wait for this batch of 100 to finish
+	                 session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
+	                 i += nextBatchLength;
+
+	                 System.out.println(
+							"DistributedPerspectiveImagePreRenderer: DistributedPerspectiveImagePreRenderer: processed through " + i + " of " + fileList.length);
+                 }
+                 System.out.println ("Number of jobs completed = " + i);
+
+                 session.deleteJobTemplate(jt);
+                 session.exit();
+
+             }
+             catch (DrmaaException e)
+             {
+                 System.out.println("Error: "  + e.getMessage());
+//                 session.exit();
+             }
+         }
+         java.awt.Toolkit.getDefaultToolkit().beep();
+         System.out.println("Done");
+
     }
 
     //String inputDirectory, ImageSource, ShapeModelBody, ShapeModelType, int imagerIndex, String outputDir = args[5] + args[1], reproecess
 
     public static void main(String[] args)
     {
+    	//$baseDir/images GASKELL RQ36 ALTWG-SPC-v20181109b 0 $baseDir/support $reprocess
         String inputDirectory = args[0];
-//        final ImageSource source = ImageSource.valueOf(args[1]);
-//        ShapeModelBody body = ShapeModelBody.valueOf(args[2]);
-//        ShapeModelType type = ShapeModelType.valueOf(args[3]);
-//        int imagerIndex = Integer.parseInt(args[4]);
-//        String outputDirectory = args[5] + "/" + args[1];
-//        boolean reprocess = Boolean.parseBoolean(args[6]);
+        final ImageSource source = ImageSource.valueOf(args[1]);
+        ShapeModelBody body = ShapeModelBody.valueOf(args[2]);
+        ShapeModelType type = ShapeModelType.valueOf(args[3]);
+        int imagerIndex = Integer.parseInt(args[4]);
+        String outputDirectory = args[5] + "/" + args[1];
+        boolean reprocess = Boolean.parseBoolean(args[6]);
 
-        SessionFactory factory = SessionFactory.getFactory();
-        Session session = factory.getSession();
-        {
-            try
-            {
-                session.init("");
-                JobTemplate jt = session.createJobTemplate();
-//                String dateString = new SimpleDateFormat("yyyyMMdd").format(new Date());
-//                File outputDir = new File(outputPath);
-//                if (!outputDir.exists()) outputDir.mkdirs();
-//                jt.setOutputPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
-//                jt.setErrorPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
-                File[] fileList;
-                fileList = new File(inputDirectory).listFiles(new FilenameFilter()
-                {
-                    @Override
-                    public boolean accept(File dir, String name)
-                    {
-                        return FilenameUtils.getExtension(name).contains("fit");
-                    }
-                });
-                for (File file : fileList)
-                {
-                    String[] tempArgs = args;
-                    tempArgs[0] = file.getAbsolutePath();
-                    DistributedPerspectiveImagePreRenderer.processImage(tempArgs, jt, session);
-                }
-
-                session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
-
-                session.deleteJobTemplate(jt);
-                session.exit();
-
-            }
-            catch (DrmaaException e)
-            {
-                System.out.println("Error: "  + e.getMessage());
-            }
-        }
+        new DistributedPerspectiveImagePreRenderer(inputDirectory, outputDirectory, source, imagerIndex, body, type, reprocess);
+        System.out.println("Done");
     }
 
     private static void processImage(String[] args, JobTemplate jt, Session session) throws DrmaaException
@@ -134,3 +109,53 @@ public class DistributedPerspectiveImagePreRenderer
     }
 
 }
+
+
+
+
+
+// TODO Auto-generated constructor stub
+
+//
+//
+//SessionFactory factory = SessionFactory.getFactory();
+//Session session = factory.getSession();
+//{
+//    try
+//    {
+//        session.init("");
+//        JobTemplate jt = session.createJobTemplate();
+//        String dateString = new SimpleDateFormat("yyyyMMdd").format(new Date());
+//        File outputDir = new File(outputPath);
+//        if (!outputDir.exists()) outputDir.mkdirs();
+//        jt.setOutputPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
+//        jt.setErrorPath(":" + JobTemplate.HOME_DIRECTORY + outputPath);
+//
+////        for (int i=-1; i<20; i++)
+//////            if ((i!=6) && (i!=7))
+////                runReport(i, jt, session);
+////        session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
+//////        runReport(6, jt, session);
+//////        runReport(7, jt, session);
+//////        session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
+////        jt.setRemoteCommand(scriptDir + "/report2.sh");           //script directory
+//        List<String> list = new ArrayList<String>();
+//        list.add(e)
+////        list.add(jarDir);                                      //jar Directory
+////        list.add(workDir);                                      //workDirectory
+////        list.add(runName);
+////        list.add(resName);                                      //resName
+//        jt.setArgs(list);
+//        String id = session.runJob(jt);
+//        System.out.println("Your job has been submitted with id " + id + " for summary reports");
+//        session.synchronize(Collections.singletonList(Session.JOB_IDS_SESSION_ALL), Session.TIMEOUT_WAIT_FOREVER, false);
+//        session.deleteJobTemplate(jt);
+//        session.exit();
+//
+//    }
+//    catch (DrmaaException e)
+//    {
+//        System.out.println("Error: "  + e.getMessage());
+//        session.exit();
+//    }
+//}
