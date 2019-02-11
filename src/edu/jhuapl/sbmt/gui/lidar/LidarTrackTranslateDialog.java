077ae6e40855b1a56cd629a4e18148927b5a1061
lopeznr1 <lopeznr1@jhuapl.edu>
2019-02-11 14:04:48 -0500

    Complete rewrite of lidar track translate dialog.
    - Changed action buttons to be: Apply, Reset, Close.
    - Support to translate a selection of lidar Tracks.
    - Added support to provide contextual error notification.
    -- 'Apply' button will be disabled on bad input
    -- Bad input will be highlighted in red.
    - Input to be prepopulated with Track translation vector (assuming all
    Tracks to be translated have the same translation vector).

---
+++ src/edu/jhuapl/sbmt/gui/lidar/LidarTrackTranslateDialog.java
@@ -0,0 +1,173 @@
+package edu.jhuapl.sbmt.gui.lidar;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+import edu.jhuapl.saavtk.gui.GNumberField;
+import edu.jhuapl.saavtk.gui.GuiUtil;
+import edu.jhuapl.sbmt.model.lidar.LidarSearchDataCollection;
+
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * Dialog which allows the user to specify the translation vector for lidar
+ * Tracks.
+ * <P>
+ * The dialog has the following action buttons:
+ * <UL>
+ * <LI>Apply: Apply user specified input.
+ * <LI>Reset: Reset user input to the zero vector and apply the changes.
+ * <LI>Close: Dismiss the dialog.
+ * </UL>
+ */
+public class LidarTrackTranslateDialog extends JDialog implements ActionListener
+{
+	// Reference vars
+	private LidarSearchDataCollection refModel;
+
+	// GUI vars
+	private JButton applyB;
+	private JButton resetB;
+	private JButton closeB;
+	private GNumberField xTranslateNF;
+	private GNumberField yTranslateNF;
+	private GNumberField zTranslateNF;
+
+	// State vars
+	private int[] idxArr;
+
+	/**
+	 * Standard Constructor
+	 */
+	public LidarTrackTranslateDialog(Component aParent, LidarSearchDataCollection aModel)
+	{
+		super(JOptionPane.getFrameForComponent(aParent));
+
+		refModel = aModel;
+
+		idxArr = new int[0];
+
+		formGui();
+		pack();
+		setLocationRelativeTo(aParent);
+	}
+
+	/**
+	 * Sets in the indexes corresponding to the tracks that will need to be
+	 * updated.
+	 */
+	public void setTracks(int[] aIdArr)
+	{
+		idxArr = aIdArr;
+
+		Vector3D tmpVect = refModel.getTranslation(aIdArr[0]);
+
+		// Check that all translation vectors are equal
+		boolean tmpBool = true;
+		for (int aId : aIdArr)
+		{
+			Vector3D evalVect = refModel.getTranslation(aId);
+			tmpBool &= tmpVect.equals(evalVect);
+		}
+
+		// Install the appropriate vector
+		if (tmpBool == false)
+			tmpVect = Vector3D.NaN;
+		setInputVector(tmpVect);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent aEvent)
+	{
+		Object source = aEvent.getSource();
+		if (source == applyB)
+			doActionApply();
+		if (source == resetB)
+			doActionReset();
+		if (source == closeB)
+			setVisible(false);
+
+		updateGui();
+	}
+
+	/**
+	 * Helper method that handles the apply action.
+	 */
+	private void doActionApply()
+	{
+		double xVal = xTranslateNF.getValue();
+		double yVal = yTranslateNF.getValue();
+		double zVal = zTranslateNF.getValue();
+		Vector3D tmpVect = new Vector3D(xVal, yVal, zVal);
+		refModel.setTranslation(idxArr, tmpVect);
+	}
+
+	/**
+	 * Helper method that handles the reset action.
+	 */
+	private void doActionReset()
+	{
+		Vector3D tmpVect = Vector3D.ZERO;
+		setInputVector(tmpVect);
+		refModel.setTranslation(idxArr, tmpVect);
+	}
+
+	/**
+	 * Helper method that forms the GUI.
+	 */
+	private void formGui()
+	{
+		setTitle("Translate Lidar Tracks");
+		setLayout(new MigLayout());
+
+		// Input area: x, y, z
+		xTranslateNF = new GNumberField(this);
+		yTranslateNF = new GNumberField(this);
+		zTranslateNF = new GNumberField(this);
+		add(new JLabel("x-translate:"), "");
+		add(xTranslateNF, "growx,span,wrap");
+		add(new JLabel("y-translate:"), "");
+		add(yTranslateNF, "growx,span,wrap");
+		add(new JLabel("z-translate:"), "");
+		add(zTranslateNF, "growx,span,wrap");
+
+		// Action area: Apply, Reset, Close
+		applyB = GuiUtil.formButton(this, "Apply");
+		resetB = GuiUtil.formButton(this, "Reset");
+		closeB = GuiUtil.formButton(this, "Close");
+		add(applyB, "span,split,align right");
+		add(resetB);
+		add(closeB);
+	}
+
+	/**
+	 * Sets our internal UI input fields to reflect the specified Vector.
+	 */
+	private void setInputVector(Vector3D aVect)
+	{
+		xTranslateNF.setValue(aVect.getX());
+		yTranslateNF.setValue(aVect.getY());
+		zTranslateNF.setValue(aVect.getZ());
+		updateGui();
+	}
+
+	/**
+	 * Helper method to keep the GUI synchornized
+	 */
+	private void updateGui()
+	{
+		boolean tmpBool = xTranslateNF.isValidInput();
+		tmpBool &= yTranslateNF.isValidInput();
+		tmpBool &= zTranslateNF.isValidInput();
+		applyB.setEnabled(tmpBool);
+	}
+
+}
