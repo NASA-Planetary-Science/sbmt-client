27aefbdfeca1999307bfce867b6064e8e1487afa
James Peachey <James.Peachey@jhuapl.edu>
2019-05-24 07:52:41 -0400

    Shut down the thread pool whose only purpose is to kill splash.

--- src/edu/jhuapl/sbmt/client/SbmtMultiMissionTool.java
+++ src/edu/jhuapl/sbmt/client/SbmtMultiMissionTool.java
@@ -359,6 +359,7 @@ public class SbmtMultiMissionTool
                         });
                     }
                 });
+                executor.shutdown();
             });
         }
         catch (Exception e)
