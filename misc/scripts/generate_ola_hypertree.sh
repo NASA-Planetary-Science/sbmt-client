2aa3b5b9c42b549f46f59e7052a94f70a037620f
Josh Steele <josh.steele@jhuapl.edu>
2019-05-31 10:46:41 -0400

    HOTFIX: slightly updated script for testing and better execution

--- misc/scripts/generate_ola_hypertree.sh
+++ misc/scripts/generate_ola_hypertree.sh
@@ -10,16 +10,24 @@ START=$2
 STOP=$3
 
 # make directory to store tree and empty if it already exists
-TREEDIR=/project/sbmt2/prod/bennu/shared/ola/search/$NAME.tmp/
+ROOTDIR=/homes/sbmt/workspace/olatest
+#ROOTDIR=/project/sbmt2/prod/bennu/shared/ola/search
+TREEDIR=$ROOTDIR/$NAME.tmp
 mkdir $TREEDIR
 rm -rf $TREEDIR/*
 
 # generate the hypertree
-./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator /project/sbmt2/prod/bennu/shared/ola/dataDirList.txt $TREEDIR OLA $START $STOP
+#/homes/sbmt/workspace/sbmt/misc/scripts/run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator /project/sbmt2/prod/bennu/shared/ola/dataDirList.txt $TREEDIR/ OLA $START $STOP
+/homes/sbmt/workspace/sbmt/misc/scripts/run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeGenerator ./dataDirList.txt $TREEDIR/ OLA $START $STOP
 
 #condense the hypertree into a bounds file .spectra
-./run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeCondenser $TREEDIR
+/homes/sbmt/workspace/sbmt/misc/scripts/run-on-linux.sh edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeCondenser $TREEDIR/
 
-mv $TREEDIR /project/sbmt2/prod/bennu/shared/ola/search/$NAME/
-
-
+#mv $TREEDIR /project/sbmt2/prod/bennu/shared/ola/search/$NAME/
+if [ -d "$ROOTDIR/$NAME" ]; then
+        mv $ROOTDIR/$NAME $ROOTDIR/$NAME.old
+fi
+mv $TREEDIR $ROOTDIR/$NAME
+if [ -d "$ROOTDIR/$NAME.old" ]; then
+        rm -rf $ROOTDIR/$NAME.old
+fi
