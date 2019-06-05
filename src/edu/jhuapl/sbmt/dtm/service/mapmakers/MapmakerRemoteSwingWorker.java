fea1a40ab17b3528e6c3039de291e2ab44e931f2
Josh Steele <josh.steele@jhuapl.edu>
2019-06-05 10:01:16 -0400

    added missing library to manifest

--- src/edu/jhuapl/sbmt/dtm/service/mapmakers/MapmakerRemoteSwingWorker.java
+++ src/edu/jhuapl/sbmt/dtm/service/mapmakers/MapmakerRemoteSwingWorker.java
@@ -3,7 +3,6 @@ package edu.jhuapl.sbmt.dtm.service.mapmakers;
 import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 import edu.jhuapl.saavtk.gui.FileDownloadSwingWorker;
 import edu.jhuapl.saavtk.util.LatLon;
@@ -210,13 +209,13 @@ public class MapmakerRemoteSwingWorker extends FileDownloadSwingWorker
     protected void done()
     {
     	// TODO Auto-generated method stub
-    	try {
-            get();
-        } catch (final InterruptedException ex) {
-            throw new RuntimeException(ex);
-        } catch (final ExecutionException ex) {
-            throw new RuntimeException(ex.getCause());
-        }
+//    	try {
+//            get();
+//        } catch (final InterruptedException ex) {
+//            throw new RuntimeException(ex);
+//        } catch (final ExecutionException ex) {
+//            throw new RuntimeException(ex.getCause());
+//        }
     	super.done();
     	System.out.println("MapmakerRemoteSwingWorker: done: done called");
     }
