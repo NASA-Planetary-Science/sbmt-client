1bbf240dce69015d6337a52f52f15b8ad47c9727
Josh Steele <josh.steele@jhuapl.edu>
2019-08-21 16:43:25 -0400

    server debugging

--- src/edu/jhuapl/sbmt/tools/DatabaseGeneratorSql.java
+++ src/edu/jhuapl/sbmt/tools/DatabaseGeneratorSql.java
@@ -646,7 +646,10 @@ public class DatabaseGeneratorSql
         {
         	System.out.println("DatabaseGeneratorSql: main: body " + bodyName + " image source " + mode + " instrument " + instrumentString);
         	System.out.println("DatabaseGeneratorSql: main: run info " + ri.toString());
-        	if (ri.name != ShapeModelBody.valueOf(bodyName).toString() || (ri.imageSource != mode) || (!ri.instrument.toString().equals(instrumentString))) continue;
+        	System.out.println("DatabaseGeneratorSql: main: " + !ri.name.equals(ShapeModelBody.valueOf(bodyName).toString()));
+        	System.out.println("DatabaseGeneratorSql: main: " + (ri.imageSource != mode));
+        	System.out.println("DatabaseGeneratorSql: main: " + (!ri.instrument.toString().equals(instrumentString)));
+        	if (!ri.name.equals(ShapeModelBody.valueOf(bodyName).toString()) || (ri.imageSource != mode) || (!ri.instrument.toString().equals(instrumentString))) continue;
             DatabaseGeneratorSql generator = new DatabaseGeneratorSql(config, ri.databasePrefix, appendTables, modifyMain, ri.instrument);
 
             String pathToFileList = ri.pathToFileList;
