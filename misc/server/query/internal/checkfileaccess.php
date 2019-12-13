65b003366c0e453e826527eb6c1d15ba972c2083
James Peachey <James.Peachey@jhuapl.edu>
2019-12-13 12:44:25 -0500

    RM 1422: add stdin argument to hold names of files to check.

--- misc/server/query/internal/checkfileaccess.php
+++ misc/server/query/internal/checkfileaccess.php
@@ -5,6 +5,7 @@ $rootURL=$_POST['rootURL'];
 $userName=$_POST['userName'];
 $password=$_POST['password'];
 $args=$_POST['args'];
+$stdin=$_POST['stdin'];
 
 $cmd = "QueryFileAccessibility.sh $rootURL $userName $password $args";
 
@@ -17,7 +18,7 @@ $process = proc_open($cmd, $descriptorspec, $pipes);
 
 if (is_resource($process)) {
     
-    fwrite($pipes[0], $fileList);
+    fwrite($pipes[0], $stdin);
     fclose($pipes[0]);
     
     $content = stream_get_contents($pipes[1]);
