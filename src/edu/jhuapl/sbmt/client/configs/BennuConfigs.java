850e333ed435cee96e921345808912e9c9086b2b
Josh Steele <josh.steele@jhuapl.edu>
2020-06-17 11:17:01 -0400

    Updated custom body cube size for bennu 1027

--- src/edu/jhuapl/sbmt/client/configs/BennuConfigs.java
+++ src/edu/jhuapl/sbmt/client/configs/BennuConfigs.java
@@ -3314,6 +3314,8 @@ public class BennuConfigs extends SmallBodyViewConfig
             c.useMinimumReferencePotential = true;
             c.rotationRate = 4.0626E-4;
             c.bodyReferencePotential = -0.02517940647257273;
+            c.hasCustomBodyCubeSize = true;
+            c.customBodyCubeSize = 0.02; //km
 
             c.hasImageMap = true;
 
