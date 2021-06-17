7512580eaa8dd557c4b688a24b14d108bb7014fc
Josh Steele <josh.steele@jhuapl.edu>
2021-06-17 13:08:40 -0400

    removed unneeded flags for linux

--- config/generate_runsbmt.sh
+++ config/generate_runsbmt.sh
@@ -42,6 +42,6 @@ DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
 export LD_LIBRARY_PATH=\"\$DIR/lib/linux64\":\$LD_LIBRARY_PATH
 export LC_NUMERIC=\"en_US.UTF-8\"
 MEMSIZE=\`grep MemTotal /proc/meminfo | awk '{print \$2}'\`
-\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64\" --add-exports java.desktop/com.sun.imageio.spi=ALL-UNNAMED --add-exports java.desktop/com.apple.laf=ALL-UNNAMED --add-exports java.desktop/apple.laf=ALL-UNNAMED -jar \"\$DIR/lib/near.jar\" \$@ &
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64\" --add-exports java.desktop/com.sun.imageio.spi=ALL-UNNAMED -jar \"\$DIR/lib/near.jar\" \$@ &
 " > $output_dir/linux64/sbmt/runsbmt
 chmod +x $output_dir/linux64/sbmt/runsbmt
