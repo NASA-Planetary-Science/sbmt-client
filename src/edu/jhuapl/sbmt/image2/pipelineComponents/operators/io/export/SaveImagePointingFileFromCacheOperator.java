cdd8f02eefb2fa8db9509239394a5632f8eded6f
Josh Steele <josh.steele@jhuapl.edu>
2022-11-03 23:00:24 -0400

    RM 1215-2356: Fixes export pointing menu item

--- src/edu/jhuapl/sbmt/image2/pipelineComponents/operators/io/export/SaveImagePointingFileFromCacheOperator.java
+++ src/edu/jhuapl/sbmt/image2/pipelineComponents/operators/io/export/SaveImagePointingFileFromCacheOperator.java
@@ -2,18 +2,15 @@ package edu.jhuapl.sbmt.image2.pipelineComponents.operators.io.export;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 import javax.swing.JOptionPane;
 
-import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.FileUtils;
 
-import edu.jhuapl.saavtk.gui.dialog.CustomFileChooser;
-import edu.jhuapl.sbmt.core.image.ImageSource;
-import edu.jhuapl.sbmt.core.image.InfoFileWriter;
+import edu.jhuapl.saavtk.gui.dialog.DirectoryChooser;
+import edu.jhuapl.saavtk.util.FileCache;
 import edu.jhuapl.sbmt.image2.interfaces.IPerspectiveImage;
 import edu.jhuapl.sbmt.image2.interfaces.IPerspectiveImageTableRepresentable;
-import edu.jhuapl.sbmt.image2.pipelineComponents.pipelines.perspectiveImages.PerspectiveImageToRenderableImagePipeline;
 import edu.jhuapl.sbmt.pipeline.operator.BasePipelineOperator;
 
 public class SaveImagePointingFileFromCacheOperator<G1 extends IPerspectiveImage & IPerspectiveImageTableRepresentable> extends BasePipelineOperator<G1, File>
@@ -25,17 +22,28 @@ public class SaveImagePointingFileFromCacheOperator<G1 extends IPerspectiveImage
 		File file = null;
 		try
 		{
-			String defaultFileName = FilenameUtils.getBaseName(inputs.get(0).getPointingSource());
-			String defaultFileType = inputs.get(0).getPointingSourceType() == ImageSource.GASKELL ? "SUM" : "INFO";
-			file = CustomFileChooser.showSaveDialog(null, "Save Pointing file as...", defaultFileName + "." + defaultFileType);
-			if (file == null) return;
+//			String defaultFileName = FilenameUtils.getBaseName(inputs.get(0).getPointingSource());
+//			String defaultFileType = inputs.get(0).getPointingSourceType() == ImageSource.GASKELL ? "SUM" : "INFO";
+//			file = CustomFileChooser.showSaveDialog(null, "Save Pointing file as...", defaultFileName + "." + defaultFileType);
+//			if (file == null) return;
+//
+//			String filename = file.getAbsolutePath();
+//
+//			PerspectiveImageToRenderableImagePipeline pipeline = new PerspectiveImageToRenderableImagePipeline(List.of(inputs.get(0)));
+//			InfoFileWriter writer = new InfoFileWriter(filename, pipeline.getRenderableImages().get(0).getPointing(), false);
+//			writer.write();
+//			outputs.add(file);
 
-			String filename = file.getAbsolutePath();
+			File outDir = DirectoryChooser.showOpenDialog(null, "Save Pointing file to Directory...");
+			if (outDir == null)
+				return;
 
-			PerspectiveImageToRenderableImagePipeline pipeline = new PerspectiveImageToRenderableImagePipeline(List.of(inputs.get(0)));
-			InfoFileWriter writer = new InfoFileWriter(filename, pipeline.getRenderableImages().get(0).getPointing(), false);
-			writer.write();
-			outputs.add(file);
+			String pointingFileName = inputs.get(0).getPointingSource();
+			File pointingFile = FileCache.getFileFromServer(pointingFileName);
+			File newPointingFile = new File(outDir, pointingFile.getName());
+			System.out.println("SaveImagePointingFileFromCacheOperator: processData: new pointing file " + newPointingFile.getAbsolutePath());
+			FileUtils.copyFile(pointingFile, newPointingFile);
+
 		}
 		catch (Exception ex)
 		{
