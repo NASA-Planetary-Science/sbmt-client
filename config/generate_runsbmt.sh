4dd786611418ff1132ee628ec6ca08458b565043
Josh Steele <josh.steele@jhuapl.edu>
2022-11-07 12:03:08 -0500

    RM 1215: fixed a missing quote

--- config/generate_runsbmt.sh
+++ config/generate_runsbmt.sh
@@ -34,7 +34,7 @@ DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
 export DYLD_LIBRARY_PATH=\"\$DIR/lib/mac64\":\"\$GDAL_DIR\":\$DYLD_LIBRARY_PATH
 export LC_NUMERIC=\"en_US.UTF-8\"
 MEMSIZE=\`sysctl hw.memsize | awk '{print int(\$2/1024)}'\`
-\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/mac64:\$GDAL_DIR\" -Dedu.jhuapl.sbmt.mission="${mission} --add-exports java.desktop/com.sun.imageio.spi=ALL-UNNAMED --add-exports java.desktop/com.apple.laf=ALL-UNNAMED --add-exports java.desktop/apple.laf=ALL-UNNAMED -jar \"\$DIR/lib/near.jar\" \$@ &
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/mac64:\$GDAL_DIR\" -Dedu.jhuapl.sbmt.mission="${mission}" --add-exports java.desktop/com.sun.imageio.spi=ALL-UNNAMED --add-exports java.desktop/com.apple.laf=ALL-UNNAMED --add-exports java.desktop/apple.laf=ALL-UNNAMED -jar \"\$DIR/lib/near.jar\" \$@ &
 " > $output_dir/mac64/sbmt/runsbmt
 chmod +x $output_dir/mac64/sbmt/runsbmt
 
@@ -44,6 +44,6 @@ DIR=\"\`(cd \"\$DIR\"; pwd)\`\"
 export LD_LIBRARY_PATH=\"\$DIR/lib/linux64\":\"\$GDAL_DIR\":\$LD_LIBRARY_PATH
 export LC_NUMERIC=\"en_US.UTF-8\"
 MEMSIZE=\`grep MemTotal /proc/meminfo | awk '{print \$2}'\`
-\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64:\$GDAL_DIR\" -Dedu.jhuapl.sbmt.mission="${mission} --add-exports java.desktop/com.sun.imageio.spi=ALL-UNNAMED -jar \"\$DIR/lib/near.jar\" \$@ &
+\"\$DIR/jre/bin/java\" -Xmx\${MEMSIZE}K -Djava.library.path=\"\$DIR/lib/linux64:\$GDAL_DIR\" -Dedu.jhuapl.sbmt.mission="${mission}" --add-exports java.desktop/com.sun.imageio.spi=ALL-UNNAMED -jar \"\$DIR/lib/near.jar\" \$@ &
 " > $output_dir/linux64/sbmt/runsbmt
 chmod +x $output_dir/linux64/sbmt/runsbmt
