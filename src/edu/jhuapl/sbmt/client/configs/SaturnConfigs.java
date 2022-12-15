0cf4d96156a3efcfc6469268f70efa86814b182a
Josh Steele <josh.steele@jhuapl.edu>
2022-12-15 09:28:24 -0500

    RM 2471: Added additional fill value for Helene

--- src/edu/jhuapl/sbmt/client/configs/SaturnConfigs.java
+++ src/edu/jhuapl/sbmt/client/configs/SaturnConfigs.java
@@ -709,7 +709,7 @@ public class SaturnConfigs extends SmallBodyViewConfig
                     BodyType.PLANETS_AND_SATELLITES.name(), //
                     ShapeModelPopulation.SATURN.name()).build(); //
             ImagingInstrument imagingInstrument = setupImagingInstrument(bodyConfig, gaskellModelConfig, Instrument.IMAGING_DATA, gaskellImagingSource, ImageType.SATURN_MOON_IMAGE);
-            imagingInstrument.setFillValues(Set.of((float)3.4028234663852886e38));
+            imagingInstrument.setFillValues(Set.of((float)3.4028234663852886e38, (float)-3.4028234663852886e38));
             c = new SaturnConfigs();
             c.body = ShapeModelBody.HELENE;
             c.type = BodyType.PLANETS_AND_SATELLITES;
