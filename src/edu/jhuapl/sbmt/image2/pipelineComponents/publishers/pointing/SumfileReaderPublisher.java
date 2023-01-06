6620ef711587879040803f758de70cc467a85a32
Josh Steele <josh.steele@jhuapl.edu>
2023-01-06 11:47:40 -0500

    RM 1215-2356: Updated SumfileReaderPublisher

--- src/edu/jhuapl/sbmt/image2/pipelineComponents/publishers/pointing/SumfileReaderPublisher.java
+++ src/edu/jhuapl/sbmt/image2/pipelineComponents/publishers/pointing/SumfileReaderPublisher.java
@@ -1,8 +1,10 @@
 package edu.jhuapl.sbmt.image2.pipelineComponents.publishers.pointing;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import edu.jhuapl.saavtk.util.FileCache;
 import edu.jhuapl.sbmt.core.image.PointingFileReader;
 import edu.jhuapl.sbmt.core.image.SumFileReader;
 import edu.jhuapl.sbmt.pipeline.publisher.BasePipelinePublisher;
@@ -12,6 +14,7 @@ public class SumfileReaderPublisher extends BasePipelinePublisher<PointingFileRe
 
 	public SumfileReaderPublisher(String filename) throws NumberFormatException, IOException
 	{
+		if (new File(filename).exists() == false) filename = FileCache.getFileFromServer(filename).getAbsolutePath();
 		SumFileReader reader = new SumFileReader(filename);
 		reader.read();
 		this.outputs = List.of(reader);
