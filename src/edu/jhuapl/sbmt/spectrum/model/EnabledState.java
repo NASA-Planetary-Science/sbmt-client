169c5b09732cfad45ed3e21c2cfe6509053bfe34
Josh Steele <josh.steele@jhuapl.edu>
2023-07-17 16:41:50 -0400

    RM 2558 - moved spectrum to own project

--- src/edu/jhuapl/sbmt/spectrum/model/EnabledState.java
+++
@@ -1,8 +0,0 @@
-package edu.jhuapl.sbmt.spectrum.model;
-
-public enum EnabledState
-{
-	ALL,
-	PARTIAL,
-	NONE;
-}
--- src/edu/jhuapl/sbmt/spectrum/service/SpectrumSearchModelBuilder.java
+++
@@ -1,9 +0,0 @@
-package edu.jhuapl.sbmt.spectrum.service;
-
-import edu.jhuapl.sbmt.spectrum.model.core.interfaces.ISpectrumSearchModel;
-
-@FunctionalInterface
-public interface SpectrumSearchModelBuilder
-{
-	ISpectrumSearchModel buildSearchModel(double diagonalLength);
-}
